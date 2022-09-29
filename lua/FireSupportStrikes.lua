567073ea384f9f6a0923334d97d0ff409ff4ee50
CDRMV <46399086+CDRMV@users.noreply.github.com>
2022-09-29 16:13:17 +0200

    Add Artillery Strike Prototyp

---
+++ lua/FireSupportStrikes.lua
@@ -0,0 +1,13 @@
+local WeaponFile = import('/lua/sim/DefaultWeapons.lua')
+local CollisionBeams = import('/lua/defaultcollisionbeams.lua')
+local BareBonesWeapon = WeaponFile.BareBonesWeapon
+local DefaultProjectileWeapon = WeaponFile.DefaultProjectileWeapon
+local DefaultBeamWeapon = WeaponFile.DefaultBeamWeapon
+local GinsuCollisionBeam = CollisionBeams.GinsuCollisionBeam
+local OrbitalDeathLaserCollisionBeam = CollisionBeams.OrbitalDeathLaserCollisionBeam
+local EffectTemplate = import('/lua/EffectTemplates.lua')
+
+
+TIFMediumArtilleryStrike = Class(DefaultProjectileWeapon) {
+    
+}
