21705de793510c30d218104e4d97c8d46467a719
CDRMV <46399086+CDRMV@users.noreply.github.com>
2022-12-08 15:53:03 +0100

    Add functional Vulcano Prototype

---
+++ units/Fire Support/Terraform/UTX0400/UTX0400_script.lua
@@ -0,0 +1,49 @@
+#****************************************************************************
+#**
+#**  UEF Medium Artillery Strike
+#**  Author(s):  CDRMV
+#**
+#**  Summary  :  A Dummy Unit which fires Artillery Strikes below it. 
+#**				 It is Selectable and Untargetable by enemy Units.				
+#**				 It attacks enemy Units automatically in Range and will be destroyed after 10 Seconds.
+#**              
+#**  Copyright © 2022 Fire Support Manager by CDRMV
+#****************************************************************************
+
+local StructureUnit = import('/lua/defaultunits.lua').StructureUnit
+local DefaultProjectileWeapon = import('/lua/sim/DefaultWeapons.lua').DefaultProjectileWeapon
+local ModEffectpath = '/mods/Commander Survival Kit/effects/emitters/'
+local R, Ceil = Random, math.ceil
+UTX0400 = Class(StructureUnit) {
+    Weapons = {
+        Turret01 = Class(DefaultProjectileWeapon) {},
+    },
+	
+    OnStopBeingBuilt = function(self,builder,layer)
+        StructureUnit.OnStopBeingBuilt(self,builder,layer)
+        self.Effect1 = CreateAttachedEmitter(self,'Crater',self:GetArmy(), ModEffectpath .. 'vulcano_smoke_01_emit.bp'):ScaleEmitter(10):OffsetEmitter(0,-10,0)
+        self.Trash:Add(self.Effect1)
+		self.Effect2 = CreateAttachedEmitter(self,'Eruption1',self:GetArmy(), ModEffectpath .. 'lava_fontaene_01_emit.bp'):ScaleEmitter(7):OffsetEmitter(0,-5,0)
+        self.Trash:Add(self.Effect2)
+		self:ForkThread(
+            function()
+                self.AimingNode = CreateRotator(self, 0, 'x', 0, 10000, 10000, 1000)
+                WaitFor(self.AimingNode)
+				local interval = 0
+                while (interval < 16) do
+				LOG(interval)
+					if interval == 15 then 
+						interval = 0
+					end
+                    local num = Ceil((R()+R()+R()+R()+R()+R()+R()+R()+R()+R()+R())*R(1,10))
+                    coroutine.yield(num)
+                    self:GetWeaponByLabel'Turret01':FireWeapon()
+					interval = interval + 1
+                end
+            end
+        )
+    end,
+
+}
+
+TypeClass = UTX0400
