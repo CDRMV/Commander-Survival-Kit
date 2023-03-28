1c5fb4c1e6a09de13db07ea7c9a19a8e7954f269
CDRMV <46399086+CDRMV@users.noreply.github.com>
2023-03-28 16:29:37 +0200

    Add Tooltips for the new Lobby Options (Steam).

---
+++ mods/Commander Survival Kit/lua/AI/CustomAITooltips/CSKTooltips.lua
@@ -0,0 +1,115 @@
+#****************************************************************************
+#**
+#**  File     :  /lua/AI/CustomAITooltips/CSKTooltips.lua
+#**  Author(s): CDRMV
+#**
+#**  Summary  : Utility File to insert the new Lobby Options Tooltips.
+#**
+#****************************************************************************
+
+Tooltips = {    
+	Ref_Points = {
+        title = "Reinforcements available in:",
+        description = "Set the wait time for the Reinforcement Point Generation in Minutes.",
+    },
+	Tac_Points = {
+        title = "Fire Support available in:",
+        description = "Set the wait time for the Tactical Point Generation in Minutes.",
+    },
+	lob_RefPoints_300 = {
+        title = "Reinforcements available in:",
+        description = "Set the Wait Time to 5 Minutes.",
+    },
+	lob_RefPoints_600 = {
+        title = "Reinforcements available in:",
+        description = "Set the Wait Time to 10 Minutes.",
+    },
+	lob_RefPoints_900 = {
+        title = "Reinforcements available in:",
+        description = "Set the Wait Time to 15 Minutes.",
+    },
+	lob_RefPoints_1200 = {
+        title = "Reinforcements available in:",
+        description = "Set the Wait Time to 20 Minutes.",
+    },
+	lob_RefPoints_1500 = {
+        title = "Reinforcements available in:",
+        description = "Set the Wait Time to 25 Minutes.",
+    },
+	lob_RefPoints_1800 = {
+        title = "Reinforcements available in:",
+        description = "Set the Wait Time to 30 Minutes.",
+    },
+	lob_RefPoints_2100 = {
+        title = "Reinforcements available in:",
+        description = "Set the Wait Time to 35 Minutes.",
+    },
+	lob_RefPoints_2400 = {
+        title = "Reinforcements available in:",
+        description = "Set the Wait Time to 40 Minutes.",
+    },
+	lob_RefPoints_2700 = {
+        title = "Reinforcements available in:",
+        description = "Set the Wait Time to 45 Minutes.",
+    },
+	lob_RefPoints_3000 = {
+        title = "Reinforcements available in:",
+        description = "Set the Wait Time to 50 Minutes.",
+    },
+	lob_RefPoints_3300 = {
+        title = "Reinforcements available in:",
+        description = "Set the Wait Time to 55 Minutes.",
+    },
+	lob_RefPoints_3600 = {
+        title = "Reinforcements available in:",
+        description = "Set the Wait Time to 60 Minutes.",
+    },
+	lob_TacPoints_300 = {
+        title = "Fire Support available in:",
+        description = "Set the Wait Time to 5 Minutes.",
+    },
+	lob_TacPoints_600 = {
+        title = "Fire Support available in:",
+        description = "Set the Wait Time to 10 Minutes.",
+    },
+	lob_TacPoints_900 = {
+        title = "Fire Support available in:",
+        description = "Set the Wait Time to 15 Minutes.",
+    },
+	lob_TacPoints_1200 = {
+        title = "Fire Support available in:",
+        description = "Set the Wait Time to 20 Minutes.",
+    },
+	lob_TacPoints_1500 = {
+        title = "Fire Support available in:",
+        description = "Set the Wait Time to 25 Minutes.",
+    },
+	lob_TacPoints_1800 = {
+        title = "Fire Support available in:",
+        description = "Set the Wait Time to 30 Minutes.",
+    },
+	lob_TacPoints_2100 = {
+        title = "Fire Support available in:",
+        description = "Set the Wait Time to 35 Minutes.",
+    },
+	lob_TacPoints_2400 = {
+        title = "Fire Support available in:",
+        description = "Set the Wait Time to 40 Minutes.",
+    },
+	lob_TacPoints_2700 = {
+        title = "Fire Support available in:",
+        description = "Set the Wait Time to 45 Minutes.",
+    },
+	lob_TacPoints_3000 = {
+        title = "Fire Support available in:",
+        description = "Set the Wait Time to 50 Minutes.",
+    },
+	lob_TacPoints_3300 = {
+        title = "Fire Support available in:",
+        description = "Set the Wait Time to 55 Minutes.",
+    },
+	lob_TacPoints_3600 = {
+        title = "Fire Support available in:",
+        description = "Set the Wait Time to 60 Minutes.",
+    },
+}
