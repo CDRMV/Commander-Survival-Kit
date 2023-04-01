0bad35fd72928655fbea8635a37730e3de3da006
CDRMV <46399086+CDRMV@users.noreply.github.com>
2023-04-01 14:59:06 +0200

    Rework the Drop Mechanic of the Aeon and Seraphim

--- mods/Commander Survival Kit/units/Reinforcements/Land/Aeon/UARL0307/UARL0307_script.lua
+++ mods/Commander Survival Kit/units/Reinforcements/Land/Aeon/UARL0307/UARL0307_script.lua
@@ -1,45 +1,95 @@
 #****************************************************************************
 #**
-#**  UEF Medium Artillery Strike
-#**  Author(s):  CDRMV
+#**  File     :  units/XRL0002/XRL0002_script.lua
 #**
-#**  Summary  :  A Dummy Unit which fires Artillery Strikes below it. 
-#**				 It is Selectable and Untargetable by enemy Units.				
-#**				 It attacks enemy Units automatically in Range and will be destroyed after 10 Seconds.
-#**              
-#**  Copyright © 2022 Fire Support Manager by CDRMV
+#**  Summary  :  Crab egg
+#**
+#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
 #****************************************************************************
+local ModeffectPath = '/mods/Commander Survival Kit/effects/emitters/'
+local AAirFactoryUnit = import('/lua/aeonunits.lua').AAirFactoryUnit
+local Util = import('/lua/utilities.lua')
+local RandomFloat = Util.GetRandomFloat
 
-local AirUnit = import('/lua/defaultunits.lua').AirUnit
-local DefaultProjectileWeapon = import('/lua/sim/DefaultWeapons.lua').DefaultProjectileWeapon
-local R, Ceil = Random, math.ceil
-UARL0307 = Class(AirUnit) {
+UARL0307 = Class(AAirFactoryUnit) {
 
-    Weapons = {
-        Turret01 = Class(DefaultProjectileWeapon) {},
-    },
-	
-	
     OnStopBeingBuilt = function(self, builder, layer)
-        AirUnit.OnStopBeingBuilt(self, builder, layer)
-        self:ForkThread(
-            function()
-                self.AimingNode = CreateRotator(self, 0, 'x', 90, 10000, 10000, 1000)
-                WaitFor(self.AimingNode)
-				local interval = 0
-                while (interval < 3) do
-				LOG(interval)
-					if interval == 2 then 
-						self:Destroy()	
-					end
-                    local num = Ceil((R()+R()+R()+R()+R()+R()+R()+R()+R()+R()+R())*R(1,10))
-                    coroutine.yield(num)
-                    self:GetWeaponByLabel'Turret01':FireWeapon()
-					interval = interval + 1
-                end
-            end
-        )
+        AAirFactoryUnit.OnStopBeingBuilt(self,builder,layer)
+		self:HideBone( 'Ring', true )
+        local bp = self:GetBlueprint()
+        local buildUnit = bp.Economy.BuildUnit
+        local Spinner1 = CreateRotator(self, 'Ring', 'y', nil, 0, 60, 360)
+		self.Trash:Add(Spinner1)
+        local pos = self:GetPosition()
+        
+        local aiBrain = self:GetAIBrain()
+
+        ForkThread( function()
+						WaitSeconds(1)
+						CreateLightParticle( self, 'Effect', self:GetArmy(), 3, 7, 'glow_03', 'ramp_white_01' ) 
+						self:ShowBone( 'Ring', true )
+						local Spinner1 = CreateRotator(self, 'Ring', 'y', nil, 0, 60, 360):SetTargetSpeed(0),
+						WaitSeconds(3)
+						CreateLightParticle( self, 'Effect', self:GetArmy(), 3, 7, 'glow_03', 'ramp_white_01' ) 
+						self.Effect1 = CreateAttachedEmitter(self,'Effect',self:GetArmy(), ModeffectPath .. 'aeon_teleport_01_emit.bp'):ScaleEmitter(1.45)
+						self.Trash:Add(self.Effect1)
+						self.Effect2 = CreateAttachedEmitter(self,'Effect',self:GetArmy(), ModeffectPath .. 'aeon_teleport_02_emit.bp'):ScaleEmitter(1.35)
+						self.Trash:Add(self.Effect2)
+						self.Effect3 = CreateAttachedEmitter(self,'Effect',self:GetArmy(), ModeffectPath .. 'aeon_teleport_03_emit.bp'):ScaleEmitter(1.45)
+						self.Trash:Add(self.Effect3)
+
+						WaitSeconds(10)
+                        CreateLightParticle( self, 'Effect', self:GetArmy(), 3, 7, 'glow_03', 'ramp_white_01' ) 
+						self:HideBone( 'Ring', true )
+						CreateLightParticle( self, 'Effect', self:GetArmy(), 3, 7, 'glow_03', 'ramp_white_01' ) 
+						self.Effect4 = CreateAttachedEmitter(self,'Effect',self:GetArmy(), ModeffectPath .. 'aeon_TeleportRing_01_emit.bp'):ScaleEmitter(1.15)
+						self.Trash:Add(self.Effect4)
+						self.Effect6 = CreateAttachedEmitter(self,'Effect',self:GetArmy(), ModeffectPath .. 'aeon_teleport_03_emit.bp'):ScaleEmitter(1.25)
+						self.Trash:Add(self.Effect6)
+						self.Effect7 = CreateAttachedEmitter(self,'Effect',self:GetArmy(), ModeffectPath .. 'aeon_teleport_04_emit.bp'):ScaleEmitter(1.45)
+						self.Trash:Add(self.Effect7)
+                        #CreateSlider(unit, bone, [goal_x, goal_y, goal_z, [speed,
+                        self.EggSlider = CreateSlider(self, 'Effect', 0, -30, 0, 10)
+                        self.Trash:Add(self.EggSlider)
+						WaitSeconds(5)
+                        WaitFor(self.EggSlider)
+                        
+                        self:Destroy()
+						CreateLightParticle( self, 'Effect', self:GetArmy(), 3, 7, 'glow_03', 'ramp_white_01' ) 
+						local orientation = RandomFloat(0,2*math.pi)
+						CreateDecal(pos, orientation, 'Crater01_albedo', '', 'Albedo', 4, 4, 200, 0, self:GetArmy())
+						CreateDecal(pos, orientation, 'Crater01_normals', '', 'Normals', 4, 4, 200, 0, self:GetArmy())            
+						CreateUnitHPR(
+						buildUnit,
+						aiBrain.Name,
+						pos[1], pos[2], pos[3],
+						0, 0, 0
+						)
+                    end
+                  )
+       
     end,
+    
+    
+    OnStopBuild = function(self, unitBeingBuilt, order)
+        if unitBeingBuilt:GetFractionComplete() == 1 then
+            ForkThread(function()
+                WaitSeconds(0.1)
+                self:Destroy()
+            end)
+        end
+    end,
+	
+	OnDestroy = function(self)
+        AAirFactoryUnit.OnDestroy(self)
+		self:HideBone( 'Ring', true )
+    end,
+	
+	OnKilled = function(self)
+        AAirFactoryUnit.OnKilled(self)
+		self:HideBone( 'Ring', true )
+    end,
+
 }
 
 TypeClass = UARL0307
--- mods/Commander Survival Kit/units/Reinforcements/Land/Seraphim/XSRL0303/XSRL0303_script.lua
+++ mods/Commander Survival Kit/units/Reinforcements/Land/Seraphim/XSRL0303/XSRL0303_script.lua
@@ -1,45 +1,91 @@
 #****************************************************************************
 #**
-#**  UEF Medium Artillery Strike
-#**  Author(s):  CDRMV
+#**  File     :  units/XRL0002/XRL0002_script.lua
 #**
-#**  Summary  :  A Dummy Unit which fires Artillery Strikes below it. 
-#**				 It is Selectable and Untargetable by enemy Units.				
-#**				 It attacks enemy Units automatically in Range and will be destroyed after 10 Seconds.
-#**              
-#**  Copyright © 2022 Fire Support Manager by CDRMV
+#**  Summary  :  Crab egg
+#**
+#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
 #****************************************************************************
+local ModeffectPath = '/mods/Commander Survival Kit/effects/emitters/'
+local SAirFactoryUnit = import('/lua/seraphimunits.lua').SAirFactoryUnit
+local Util = import('/lua/utilities.lua')
+local RandomFloat = Util.GetRandomFloat
 
-local AirUnit = import('/lua/defaultunits.lua').AirUnit
-local DefaultProjectileWeapon = import('/lua/sim/DefaultWeapons.lua').DefaultProjectileWeapon
-local R, Ceil = Random, math.ceil
-XSRL0303 = Class(AirUnit) {
+XSRL0303 = Class(SAirFactoryUnit) {
 
-    Weapons = {
-        Turret01 = Class(DefaultProjectileWeapon) {},
-    },
-	
-	
     OnStopBeingBuilt = function(self, builder, layer)
-        AirUnit.OnStopBeingBuilt(self, builder, layer)
-        self:ForkThread(
-            function()
-                self.AimingNode = CreateRotator(self, 0, 'x', 90, 10000, 10000, 1000)
-                WaitFor(self.AimingNode)
-				local interval = 0
-                while (interval < 3) do
-				LOG(interval)
-					if interval == 2 then 
-						self:Destroy()	
-					end
-                    local num = Ceil((R()+R()+R()+R()+R()+R()+R()+R()+R()+R()+R())*R(1,10))
-                    coroutine.yield(num)
-                    self:GetWeaponByLabel'Turret01':FireWeapon()
-					interval = interval + 1
-                end
-            end
-        )
+        SAirFactoryUnit.OnStopBeingBuilt(self,builder,layer)
+		self:HideBone( 'Ring', true )
+        local bp = self:GetBlueprint()
+        local buildUnit = bp.Economy.BuildUnit
+        local Spinner1 = CreateRotator(self, 'Ring', 'y', nil, 0, 60, 360)
+		self.Trash:Add(Spinner1)
+        local pos = self:GetPosition()
+        
+        local aiBrain = self:GetAIBrain()
+
+        ForkThread( function()
+						WaitSeconds(1)
+						CreateLightParticle( self, 'Effect', self:GetArmy(), 3, 7, 'glow_03', 'ramp_ser_01.dds' ) 
+						self.Effect1 = CreateAttachedEmitter(self,'Effect',self:GetArmy(), ModeffectPath .. 'sera_teleport_01_emit.bp'):ScaleEmitter(1.75)
+						self.Trash:Add(self.Effect1)
+						self.Effect2 = CreateAttachedEmitter(self,'Effect',self:GetArmy(), ModeffectPath .. 'sera_teleport_02_emit.bp'):ScaleEmitter(1.65)
+						self.Trash:Add(self.Effect2)
+						self.Effect3 = CreateAttachedEmitter(self,'Effect',self:GetArmy(), ModeffectPath .. 'sera_teleport_03_emit.bp'):ScaleEmitter(1.85)
+						self.Trash:Add(self.Effect3)
+
+						WaitSeconds(13)
+                        CreateLightParticle( self, 'Effect', self:GetArmy(), 3, 7, 'glow_03', 'ramp_ser_01.dds' ) 
+						--self:HideBone( 'Ring', true )
+						CreateLightParticle( self, 'Effect', self:GetArmy(), 3, 7, 'glow_03', 'ramp_ser_01.dds' ) 
+						self.Effect4 = CreateAttachedEmitter(self,'Effect',self:GetArmy(), ModeffectPath .. 'sera_TeleportRing_01_emit.bp'):ScaleEmitter(1.75)
+						self.Trash:Add(self.Effect4)
+						self.Effect6 = CreateAttachedEmitter(self,'Effect',self:GetArmy(), ModeffectPath .. 'sera_teleport_03_emit.bp'):ScaleEmitter(1.65)
+						self.Trash:Add(self.Effect6)
+						self.Effect7 = CreateAttachedEmitter(self,'Effect',self:GetArmy(), ModeffectPath .. 'sera_teleport_04_emit.bp'):ScaleEmitter(1.85)
+						self.Trash:Add(self.Effect7)
+                        #CreateSlider(unit, bone, [goal_x, goal_y, goal_z, [speed,
+                        self.EggSlider = CreateSlider(self, 'Effect', 0, -30, 0, 10)
+                        self.Trash:Add(self.EggSlider)
+						WaitSeconds(5)
+                        WaitFor(self.EggSlider)
+                        
+                        self:Destroy()
+						CreateLightParticle( self, 'Effect', self:GetArmy(), 3, 7, 'glow_03', 'ramp_ser_01.dds' ) 
+						local orientation = RandomFloat(0,2*math.pi)
+						CreateDecal(pos, orientation, 'Crater01_albedo', '', 'Albedo', 4, 4, 200, 0, self:GetArmy())
+						CreateDecal(pos, orientation, 'Crater01_normals', '', 'Normals', 4, 4, 200, 0, self:GetArmy())       
+						CreateUnitHPR(
+						buildUnit,
+						aiBrain.Name,
+						pos[1], pos[2], pos[3],
+						0, 0, 0
+						)
+                    end
+                  )
+       
     end,
+    
+    
+    OnStopBuild = function(self, unitBeingBuilt, order)
+        if unitBeingBuilt:GetFractionComplete() == 1 then
+            ForkThread(function()
+                WaitSeconds(0.1)
+                self:Destroy()
+            end)
+        end
+    end,
+	
+	OnDestroy = function(self)
+        SAirFactoryUnit.OnDestroy(self)
+		self:HideBone( 'Ring', true )
+    end,
+	
+	OnKilled = function(self)
+        SAirFactoryUnit.OnKilled(self)
+		self:HideBone( 'Ring', true )
+    end,
+
 }
 
 TypeClass = XSRL0303
