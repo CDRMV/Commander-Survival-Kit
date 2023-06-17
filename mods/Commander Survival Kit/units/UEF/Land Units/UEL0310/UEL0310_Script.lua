4a65eb63bd6fb22d3f5379d683318e6beb24c5ef
CDRMV <46399086+CDRMV@users.noreply.github.com>
2023-06-17 15:45:34 +0200

    New UEF Land Units WIP Phase 1

---
+++ mods/Commander Survival Kit/units/UEF/Land Units/UEL0310/UEL0310_Script.lua
@@ -0,0 +1,21 @@
+#****************************************************************************
+#**
+#**  File     :  /cdimage/units/UEL0304/UEL0304_script.lua
+#**  Author(s):  John Comes, David Tomandl, Jessica St. Croix
+#**
+#**  Summary  :  UEF Mobile Heavy Artillery Script
+#**
+#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
+#****************************************************************************
+
+local TLandUnit = import('/lua/terranunits.lua').TLandUnit
+local TDFGaussCannonWeapon = import('/lua/terranweapons.lua').TDFGaussCannonWeapon
+
+UEL0310 = Class(TLandUnit) {
+    Weapons = {
+        MainGun = Class(TDFGaussCannonWeapon) {
+        }
+    },
+}
+
+TypeClass = UEL0310
