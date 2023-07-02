2ac5ec44250f5f8d69c12bac522d56bcdd323866
CDRMV <46399086+CDRMV@users.noreply.github.com>
2023-07-02 13:53:16 +0200

    New Early Access Update

--- mods/Commander Survival Kit/lua/CustomUnits/CustomUnits2.lua
+++ mods/Commander Survival Kit/lua/CustomUnits/CustomUnits2.lua
@@ -13,11 +13,11 @@ UnitList = {
  	}, 
 	
 	T3AirBomber = {
-		UEF = {'CSKTA0312', 50},
+		UEF = {'CSKTA0312', 50},    -- B-1265 Preacher, Tech 3 Heavy Bomber
     },
 	
 	T3AirFighter = {
-		UEF = {'CSKTA0311', 50},    -- BX-2000 Phoenix, Experimental Bomber
+		UEF = {'CSKTA0311', 50},    -- A-50 Thunderbolt X, Groundattack Fighter
 	},
 	
 	T4AirExperimental1 = {
