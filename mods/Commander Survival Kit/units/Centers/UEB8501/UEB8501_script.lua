ca7b3fc59d28f051fac2107f039d873296e24ed2
CDRMV <46399086+CDRMV@users.noreply.github.com>
2023-07-15 13:55:39 +0200

    Add new UEF Center Models

--- mods/Commander Survival Kit/units/Centers/UEB8501/UEB8501_script.lua
+++ mods/Commander Survival Kit/units/Centers/UEB8501/UEB8501_script.lua
@@ -8,6 +8,29 @@
 local TStructureUnit = import('/lua/terranunits.lua').TStructureUnit
 
 UEB8501 = Class(TStructureUnit) {
+
+    OnCreate = function(self)
+        TStructureUnit.OnCreate(self)
+		
+		self.WindowEntity = import('/lua/sim/Entity.lua').Entity({Owner = self,})
+        self.WindowEntity:AttachBoneTo( -1, self, 'Spinner' )
+        self.WindowEntity:SetMesh('/mods/Commander Survival Kit/effects/entities/Symbols/UEF/Tactical/Tactical_mesh')
+        self.WindowEntity:SetDrawScale(0.25)
+        self.WindowEntity:SetVizToAllies('Intel')
+        self.WindowEntity:SetVizToNeutrals('Intel')
+        self.WindowEntity:SetVizToEnemies('Intel')        
+        self.Trash:Add(self.WindowEntity)
+		
+		--self.Effect1 = CreateAttachedEmitter(self,'Projector',self:GetArmy(), '/effects/emitters/aeon_t2power_ambient_02_emit.bp'):ScaleEmitter(0.5)
+        --self.Trash:Add(self.Effecct1)
+        self.Spinners = {
+            Spinner1 = CreateRotator(self, 'Spinner', 'y', nil, 0, 60, 360):SetTargetSpeed(90),
+        }
+        for k, v in self.Spinners do
+            self.Trash:Add(v)
+        end
+    end,
+
 }
 
 
