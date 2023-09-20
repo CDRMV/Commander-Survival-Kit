1cbf287487e4024a78a7a6be457e7958a68ee7d1
CDRMV <46399086+CDRMV@users.noreply.github.com>
2023-09-20 14:37:15 +0200

    Add new second Aeon Experimental (Still in Development)

---
+++ mods/Commander Survival Kit Units/units/Aeon/CSKAL0401/CSKAL0401_Script.lua
@@ -0,0 +1,37 @@
+#****************************************************************************
+#**
+#**  File     :  /cdimage/units/UAL0303/UAL0303_script.lua
+#**  Author(s):  John Comes, David Tomandl
+#**
+#**  Summary  :  Aeon Siege Assault Bot Script
+#**
+#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
+#****************************************************************************
+
+local AWalkingLandUnit = import('/lua/aeonunits.lua').AWalkingLandUnit
+local ADFLaserHighIntensityWeapon = import('/lua/aeonweapons.lua').ADFLaserHighIntensityWeapon
+local EffectUtil = import('/lua/EffectUtilities.lua')
+
+CSKAL0401 = Class(AWalkingLandUnit) {    
+    Weapons = {
+        FrontTurret01 = Class(ADFLaserHighIntensityWeapon) {}
+    },
+    
+    CreateBuildEffects = function( self, unitBeingBuilt, order )
+        EffectUtil.CreateAeonCommanderBuildingEffects( self, unitBeingBuilt, self:GetBlueprint().General.BuildBones.BuildEffectBones, self.BuildEffectsBag )
+    end,  
+	
+	OnCreate = function(self)
+        AWalkingLandUnit.OnCreate(self)
+        self:HideBone('Leg01_Turret', true)
+        self:HideBone('Leg02_Turret', true)        
+        self:HideBone('Leg03_Turret', true)   
+        self:HideBone('Leg04_Turret', true) 
+		self:HideBone('L_Turret00', true)
+        self:HideBone('R_Turret00', true)    
+        self:HideBone('L_Turret01', true)      
+        self:HideBone('R_Turret01', true)     	   		
+    end,
+}
+
+TypeClass = CSKAL0401
