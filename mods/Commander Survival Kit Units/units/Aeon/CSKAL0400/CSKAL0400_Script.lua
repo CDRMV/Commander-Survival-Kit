584771f9cd7f3a14ea84c632a318dae48b0d0b65
CDRMV <46399086+CDRMV@users.noreply.github.com>
2023-09-25 18:17:37 +0200

    Arrival Improvements (CSK Units only) + Balancing

--- mods/Commander Survival Kit Units/units/Aeon/CSKAL0400/CSKAL0400_Script.lua
+++ mods/Commander Survival Kit Units/units/Aeon/CSKAL0400/CSKAL0400_Script.lua
@@ -13,8 +13,10 @@ local ADFLaserHighIntensityWeapon = import('/lua/aeonweapons.lua').ADFLaserHighI
 local EffectUtil = import('/lua/EffectUtilities.lua')
 local Util = import('/lua/utilities.lua')
 local RandomFloat = Util.GetRandomFloat
+local EffectTemplate = import('/lua/EffectTemplates.lua')
 
-CSKAL0400 = Class(AWalkingLandUnit) {    
+CSKAL0400 = Class(AWalkingLandUnit) { 
+	decal = nil,   
     Weapons = {
         FrontTurret01 = Class(ADFLaserHighIntensityWeapon) {}
     },
@@ -23,8 +25,7 @@ CSKAL0400 = Class(AWalkingLandUnit) {
         EffectUtil.CreateAeonCommanderBuildingEffects( self, unitBeingBuilt, self:GetBlueprint().General.BuildBones.BuildEffectBones, self.BuildEffectsBag )
     end,  
 	
-	
-	CreateOuterRingWaveSmokeRing = function(self)
+	CreateOuterRingWaveSmokeRing = function(self) 
         local sides = 32
         local angle = (2*math.pi) / sides
         local velocity = 7
@@ -47,8 +48,41 @@ CSKAL0400 = Class(AWalkingLandUnit) {
         end         
     end, 
 	
+	CreateGroundPlumeConvectionEffects = function(self,army)
+    for k, v in EffectTemplate.TNukeGroundConvectionEffects01 do
+          CreateEmitterAtEntity(self, army, v ) 
+    end
+    
+    local sides = 10
+    local angle = (2*math.pi) / sides
+    local inner_lower_limit = 2
+        local outer_lower_limit = 2
+        local outer_upper_limit = 2
+    
+    local inner_lower_height = 1
+    local inner_upper_height = 3
+    local outer_lower_height = 2
+    local outer_upper_height = 3
+      
+    sides = 8
+    angle = (2*math.pi) / sides
+    for i = 0, (sides-1)
+    do
+        local magnitude = RandomFloat(outer_lower_limit, outer_upper_limit)
+        local x = math.sin(i*angle+RandomFloat(-angle/2, angle/4)) * magnitude
+        local z = math.cos(i*angle+RandomFloat(-angle/2, angle/4)) * magnitude
+        local velocity = RandomFloat( 1, 3 ) * 3
+        self:CreateProjectile('/effects/entities/UEFNukeEffect05/UEFNukeEffect05_proj.bp', x, RandomFloat(outer_lower_height, outer_upper_height), z, x, 0, z)
+            :SetVelocity(x * velocity, 0, z * velocity)
+    end 
+    end,
+	
 	OnCreate = function(self)
         AWalkingLandUnit.OnCreate(self)
+		local army = self:GetArmy()
+		local position = self:GetPosition()
+		local orientation = 0
+		decal = CreateDecal(position, orientation, '/mods/Commander Survival Kit Units/icons/aeon_symbol.dds', '', 'Albedo', 15, 15, 500, 600, army)
 		self.ArmSlider1 = CreateSlider(self, 'Body')
         self.Trash:Add(self.ArmSlider1)
 		self.ArmSlider1:SetGoal(0, 1000, 0)
@@ -63,18 +97,17 @@ CSKAL0400 = Class(AWalkingLandUnit) {
         self:HideBone('L_Turret02', true)        
         self:HideBone('R_Turret01', true)   
         self:HideBone('R_Turret02', true)   
-		--[[
-        if not self.AnimationManipulator then
-            self.AnimationManipulator = CreateAnimator(self)
-            self.Trash:Add(self.AnimationManipulator)
-        end
-        self.AnimationManipulator:PlayAnim(self:GetBlueprint().Display.AnimationActivate, false):SetRate(0)
-		]]--		
     end,
 	
 	
 	 OnStopBeingBuilt = function(self,builder,layer)
         AWalkingLandUnit.OnStopBeingBuilt(self,builder,layer)
+		decal:Destroy()
+		if not self.AnimationManipulator then
+            self.AnimationManipulator = CreateAnimator(self)
+            self.Trash:Add(self.AnimationManipulator)
+        end
+        self.AnimationManipulator:PlayAnim(self:GetBlueprint().Display.AnimationActivate, false):SetRate(0)		
 		self:DisableShield()
 		local army = self:GetArmy()
         local position = self:GetPosition()
@@ -99,11 +132,19 @@ CSKAL0400 = Class(AWalkingLandUnit) {
 		# Knockdown force rings
         DamageRing(self, position, 11, 20, 1, 'Force', false, false)
         DamageRing(self, position, 11, 20, 1, 'Force', false, false)
-        CreateDecal(position, orientation, 'nuke_scorch_002_albedo', '', 'Albedo', 28, 28, 500, 600, army)
+        CreateDecal(position, orientation, 'Scorch_010_albedo', '', 'Albedo', 28, 28, 500, 600, army)
         CreateDecal(position, orientation, 'Crater05_normals', '', 'Normals', 28, 28, 500, 600, army)
-        CreateDecal(position, orientation, 'Crater05_normals', '', 'Normals', 12, 12, 500, 600, army) 
+        CreateDecal(position, orientation, 'Crater05_normals', '', 'Normals', 12, 12, 500, 600, army)
+		if self.AnimationManipulator then
+            self:ForkThread(function()
+				self.AnimationManipulator:SetRate(0.1)
+                WaitSeconds(15)
+                self.AnimationManipulator:Destroy()
+				self:SetUnSelectable(false)	
+            end)
+        end		
+		self:CreateGroundPlumeConvectionEffects()		
 		self:CreateOuterRingWaveSmokeRing()
-        self:SetUnSelectable(false)	
 		self:EnableShield()		
         # Scorch decal and light some trees on fire
         DamageRing(self, position, 20, 27, 1, 'Fire', false, false)
@@ -115,18 +156,6 @@ CSKAL0400 = Class(AWalkingLandUnit) {
             end
         end		
 		end)	
-        --[[
-        if self.AnimationManipulator then
-            self:SetUnSelectable(true)
-            self.AnimationManipulator:SetRate(1)
-            
-            self:ForkThread(function()
-                WaitSeconds(self.AnimationManipulator:GetAnimationDuration()*self.AnimationManipulator:GetRate())
-                self:SetUnSelectable(false)
-                self.AnimationManipulator:Destroy()
-            end)
-        end
-		]]--
     end,
 }
 
