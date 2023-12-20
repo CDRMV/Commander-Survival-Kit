5923a5c4abebed75575dcfe0d079a7715f685c6a
CDRMV <46399086+CDRMV@users.noreply.github.com>
2023-12-20 02:33:53 +0100

    Cybran Worm Improvements

--- mods/Commander Survival Kit Units/units/Cybran/Land/CSKCL0402/CSKCL0402_Script.lua
+++ mods/Commander Survival Kit Units/units/Cybran/Land/CSKCL0402/CSKCL0402_Script.lua
@@ -10,16 +10,18 @@
 
 local CLandUnit = import('/lua/defaultunits.lua').MobileUnit
 local cWeapons = import('/lua/cybranweapons.lua')
-local CIFArtilleryWeapon = cWeapons.CIFArtilleryWeapon
+local CDFHeavyMicrowaveLaserGeneratorCom = cWeapons.CDFHeavyMicrowaveLaserGeneratorCom
 local RandomFloat = import('/lua/utilities.lua').GetRandomFloat
 
 
 CSKCL0402 = Class(CLandUnit) 
 {
+    Walking = false,
+    IsWaiting = false,
     PlayEndAnimDestructionEffects = false,
 
     Weapons = {
-		Gun01 = Class(CIFArtilleryWeapon) {},		
+        MainGun = Class(CDFHeavyMicrowaveLaserGeneratorCom) {},	
     },
 	
     
@@ -36,26 +38,49 @@ CSKCL0402 = Class(CLandUnit)
 		self:RemoveCommandCap('RULEUCC_Patrol')
     end,
 	
+	OnMotionHorzEventChange = function(self, new, old)
+        CLandUnit.OnMotionHorzEventChange(self, new, old)
+        if self:IsDead() then return end
+        if( self.IsWaiting ) then
+            if( self.Walking ) then
+                if( old == 'Stopped' ) then
+					local rotation = RandomFloat(0,2*math.pi)
+					local size = RandomFloat(10.75,10.0)
+					CreateDecal(self:GetPosition(), rotation, 'scorch_001_albedo', '', 'Albedo', size, size, 150, 150, self:GetArmy())
+                elseif( new == 'Stopped' ) then
+                    
+                end
+            end
+        end
+    end,
+	
 	OnScriptBitSet = function(self, bit)
         CLandUnit.OnScriptBitSet(self, bit)
         if bit == 1 then 
 		ForkThread( function()
+						self:SetPaused(true)
+						self:SetImmobile(true)
+						self:SetUnSelectable(true)
 						local rotation = RandomFloat(0,2*math.pi)
 						local size = RandomFloat(10.75,10.0)
-						CreateDecal(self:GetPosition(), rotation, 'scorch_001_albedo', '', 'Albedo', size, size, 150, 150, self:GetArmy())
-						self:SetUnSelectable(true)
-						self:SetWeaponEnabledByLabel('Gun01', false)
-						self.Effect1 = CreateAttachedEmitter(self,'CSKCL0402',self:GetArmy(), '/effects/emitters/air_hover_exhaust_01_emit.bp'):ScaleEmitter(0.15)
+						self.Effect1 = CreateAttachedEmitter(self,'CSKCL0402',self:GetArmy(), '/effects/emitters/dust_cloud_05_emit.bp'):ScaleEmitter(5):SetEmitterParam('LIFETIME', 200)
 						self.Trash:Add(self.Effect1)
+						self.Effect2 = CreateAttachedEmitter(self,'CSKCL0402',self:GetArmy(), '/effects/emitters/dust_cloud_06_emit.bp'):ScaleEmitter(10):SetEmitterParam('LIFETIME', 200):OffsetEmitter(0,-1,0)
+						self:ShakeCamera(200, 1, 0, 20)
+						self.Trash:Add(self.Effect2)
+						CreateDecal(self:GetPosition(), rotation, 'scorch_001_albedo', '', 'Albedo', size, size, 150, 150, self:GetArmy())
+						self:SetWeaponEnabledByLabel('MainGun', false)
                         self.OpenAnimManip:SetRate(-0.5)
 						WaitFor(self.OpenAnimManip)
 						self.Worm = CreateSlider(self, 'B00', 0, -400, 0, 25)
                         self.Trash:Add(self.Worm)
+						WaitFor(self.Worm)
 						self:SetUnSelectable(false)
 						self:SetDoNotTarget(true)
 						self:AddCommandCap('RULEUCC_Move')
 						self:AddCommandCap('RULEUCC_Guard')
 						self:AddCommandCap('RULEUCC_Patrol')
+						self:SetImmobile(false)
             end
         )
         end
@@ -65,23 +90,29 @@ CSKCL0402 = Class(CLandUnit)
         CLandUnit.OnScriptBitClear(self, bit)
         if bit == 1 then 
 		ForkThread( function()
+						self:SetPaused(true)
+						self:SetImmobile(true)
+						self:SetUnSelectable(true)
 						local rotation = RandomFloat(0,2*math.pi)
 						local size = RandomFloat(10.75,10.0)
+						self.Effect1 = CreateAttachedEmitter(self,'CSKCL0402',self:GetArmy(), '/effects/emitters/dust_cloud_05_emit.bp'):ScaleEmitter(5):SetEmitterParam('LIFETIME', 200)
+						self.Trash:Add(self.Effect1)
+						self.Effect2 = CreateAttachedEmitter(self,'CSKCL0402',self:GetArmy(), '/effects/emitters/dust_cloud_06_emit.bp'):ScaleEmitter(10):SetEmitterParam('LIFETIME', 200):OffsetEmitter(0,-1,0)
+						self:ShakeCamera(200, 1, 0, 20)
+						self.Trash:Add(self.Effect2)
 						CreateDecal(self:GetPosition(), rotation, 'scorch_001_albedo', '', 'Albedo', size, size, 150, 150, self:GetArmy())
-						self:SetUnSelectable(true)
 						self.Worm = CreateSlider(self, 'B00', 0, 400, 0, 25)
                         self.Trash:Add(self.Worm)
 						WaitFor(self.Worm)
-						self.Effect1 = CreateAttachedEmitter(self,'CSKCL0402',self:GetArmy(), '/effects/emitters/air_hover_exhaust_01_emit.bp'):ScaleEmitter(0.15)
-						self.Trash:Add(self.Effect1)
                         self.OpenAnimManip:SetRate(0.5)
 						WaitFor(self.OpenAnimManip)
-						self:SetWeaponEnabledByLabel('Gun01', true)
+						self:SetWeaponEnabledByLabel('MainGun', true)
 						self:SetUnSelectable(false)
 						self:SetDoNotTarget(false)
 						self:RemoveCommandCap('RULEUCC_Move')
 						self:RemoveCommandCap('RULEUCC_Guard')
 						self:RemoveCommandCap('RULEUCC_Patrol')
+						self:SetImmobile(false)
             end
         )
         end
