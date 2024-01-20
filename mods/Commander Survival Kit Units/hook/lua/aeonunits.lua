f93dbe8ef591aa68d413f61a691df49342c1602d
CDRMV <46399086+CDRMV@users.noreply.github.com>
2024-01-20 11:40:30 +0100

    Laser Fence Update

--- mods/Commander Survival Kit Units/hook/lua/aeonunits.lua
+++ mods/Commander Survival Kit Units/hook/lua/aeonunits.lua
@@ -25,15 +25,32 @@ if version < 3652 then
 ALaserFenceUnit = Class(StructureUnit) {
 	OnStopBeingBuilt = function(self, builder, layer)
         StructureUnit.OnStopBeingBuilt(self, builder, layer)
-		local bp = self:GetBlueprint()
-		local Desc = builder:GetBlueprint().Description
+		
+				local bp = self:GetBlueprint()
         local bpAnim = bp.Display.AnimationOpen
-		local buildpos = builder:GetPosition()
+								self:ForkThread(function()
+					 self.OpenAnim = CreateAnimator(self)
+					self.Trash:Add(self.OpenAnim )
+                    self.OpenAnim:PlayAnim(bpAnim)
+					WaitFor(self.OpenAnim)
+						local units = AIUtils.GetOwnUnitsAroundPoint(
+			
+			self:GetAIBrain(), 
+			categories.LASERWALL,
+			self:GetPosition(), 
+			3
+			
+			)
+            
+
+            for _,unit in units do
+			
+					local buildpos = unit:GetPosition()
 		local pos = self:GetPosition()
 		local alpha = math.atan2 (buildpos[3] - pos[3] ,buildpos[1] - pos[1])
 		local dist = VDist2(buildpos[1], buildpos[3], pos[1], pos[3])
-		--LOG('Position: ', math.cos(alpha) * dist)
-		--LOG('Builder Position: ', math.sin(alpha) * dist)
+		LOG('Position: ', math.cos(alpha) * dist)
+		LOG('Builder Position: ', math.sin(alpha) * dist)
 		
 		local CheckAlpha = math.cos(alpha) * dist
 		local CheckDist = math.sin(alpha) * dist
@@ -41,22 +58,68 @@ ALaserFenceUnit = Class(StructureUnit) {
 		if CheckAlpha < 0 and CheckDist == 2 then
 		x = buildpos[1] - math.cos(alpha) * dist
 		y = buildpos[3] - math.sin(alpha) * dist + 1
+		Dummy = CreateUnitHPR('UABAB0201a',self:GetArmy(), x, 0, y, 0, 0, 0)
 		elseif CheckAlpha < 0 and CheckDist == -2 then
 		x = buildpos[1] - math.cos(alpha) * dist
 		y = buildpos[3] - math.sin(alpha) * dist - 1
+		Dummy = CreateUnitHPR('UABAB0201a',self:GetArmy(), x, 0, y, 0, 0, 0)
 		elseif CheckAlpha == 2 and CheckDist == 0 then
 		x = buildpos[1] - math.cos(alpha) * dist + 1
 		y = buildpos[3] - math.sin(alpha) * dist
+		Dummy = CreateUnitHPR('UABAB0201a',self:GetArmy(), x, 0, y, 0, 0, 0)
 		elseif CheckAlpha == -2 and CheckDist < 0 then
 		x = buildpos[1] - math.cos(alpha) * dist - 1
 		y = buildpos[3] - math.sin(alpha) * dist
-		end
+		Dummy = CreateUnitHPR('UABAB0201a',self:GetArmy(), x, 0, y, 0, 0, 0)
+			end
+		end	
+		
+		end)
+			
+    end,
+	
+	OnKilled = function(self)
+        StructureUnit.OnKilled(self)
 		local units = AIUtils.GetOwnUnitsAroundPoint(
+			
+			self:GetAIBrain(), 
+			categories.LASERWALLDUMMY,
+			self:GetPosition(), 
+			2
+			
+			)
+            
+
+            for _,unit in units do
+			if unit == nil then
+			else
+				unit:Destroy()
+            end
+			end
+    end,
+}
+
+ALaserFenceDummyUnit = Class(StructureUnit) {
+	OnStopBeingBuilt = function(self, builder, layer)
+        StructureUnit.OnStopBeingBuilt(self, builder, layer)
+		
+		        self:HideBone( 0, true )
+		
+						local bp = self:GetBlueprint()
+        local bpAnim = bp.Display.AnimationOpen
+					self:ForkThread(function()
+					 self.OpenAnim = CreateAnimator(self)
+					self.Trash:Add(self.OpenAnim )
+                    self.OpenAnim:PlayAnim(bpAnim)
+					WaitFor(self.OpenAnim)
+		
+		
+				local units = AIUtils.GetOwnUnitsAroundPoint(
 			
 			self:GetAIBrain(), 
 			categories.LASERWALL,
 			self:GetPosition(), 
-			3
+			2
 			
 			)
             
@@ -65,53 +128,16 @@ ALaserFenceUnit = Class(StructureUnit) {
 					table.insert(ChargeEffects01Bag,AttachBeamEntityToEntity(self, 'Effect1', unit, 'Effect1', self:GetArmy(), Effects ))
 					table.insert(ChargeEffects01Bag,AttachBeamEntityToEntity(self, 'Effect2', unit, 'Effect2', self:GetArmy(), Effects ))
 					table.insert(ChargeEffects01Bag,AttachBeamEntityToEntity(self, 'Effect3', unit, 'Effect3', self:GetArmy(), Effects ))
-            end
-
-        if bpAnim then		
-			if not self.Dead then
-				    self.OpenAnim = CreateAnimator(self)
-					self.Trash:Add(self.OpenAnim )
-                    self.OpenAnim:PlayAnim(bpAnim)
-					if Desc == 'Laser Fence' then
-					--LOG('Distanz X: ', x)
-					--LOG('Distanz Y: ', y)
-					Dummy = CreateUnitHPR('UABAB0201a',self:GetArmy(), x, 0, y, 0, 0, 0)
-					else
-					end
-					
-            end
-		end
+			end
+			
+			end)
 			
     end,
 	
 	OnKilled = function(self)
         StructureUnit.OnKilled(self)
-		Dummy:Destroy()
-		if ChargeEffects01Bag then
-            for k, v in ChargeEffects01Bag do
-                v:Destroy()
-            end
-        end
+		self:Destroy()
     end,
-}
-
-ALaserFenceDummyUnit = Class(StructureUnit) {
-	OnStopBeingBuilt = function(self, builder, layer)
-        StructureUnit.OnStopBeingBuilt(self, builder, layer)
-
-			
-    end,
-	
-	OnKilled = function(self)
-        StructureUnit.OnKilled(self)
-		if ChargeEffects01Bag then
-            for k, v in ChargeEffects01Bag do
-                v:Destroy()
-            end
-        end
-    end,
-	
-	
 	
 }
 
@@ -120,33 +146,15 @@ else
 ALaserFenceUnit = Class(StructureUnit) {
 	OnStopBeingBuilt = function(self, builder, layer)
         StructureUnit.OnStopBeingBuilt(self, builder, layer)
-		local bp = self:GetBlueprint()
-		local Desc = builder:GetBlueprint().Description
+		
+				local bp = self:GetBlueprint()
         local bpAnim = bp.Display.AnimationOpen
-		local buildpos = builder:GetPosition()
-		local pos = self:GetPosition()
-		local alpha = math.atan2 (buildpos[3] - pos[3] ,buildpos[1] - pos[1])
-		local dist = VDist2(buildpos[1], buildpos[3], pos[1], pos[3])
-		--LOG('Position: ', math.cos(alpha) * dist)
-		--LOG('Builder Position: ', math.sin(alpha) * dist)
-		
-		local CheckAlpha = math.cos(alpha) * dist
-		local CheckDist = math.sin(alpha) * dist
-		
-		if CheckAlpha < 0 and CheckDist == 2 then
-		x = buildpos[1] - math.cos(alpha) * dist
-		y = buildpos[3] - math.sin(alpha) * dist + 1
-		elseif CheckAlpha < 0 and CheckDist == -2 then
-		x = buildpos[1] - math.cos(alpha) * dist
-		y = buildpos[3] - math.sin(alpha) * dist - 1
-		elseif CheckAlpha == 2 and CheckDist == 0 then
-		x = buildpos[1] - math.cos(alpha) * dist + 1
-		y = buildpos[3] - math.sin(alpha) * dist
-		elseif CheckAlpha == -2 and CheckDist < 0 then
-		x = buildpos[1] - math.cos(alpha) * dist - 1
-		y = buildpos[3] - math.sin(alpha) * dist
-		end
-		local units = AIUtils.GetOwnUnitsAroundPoint(
+								self:ForkThread(function()
+					 self.OpenAnim = CreateAnimator(self)
+					self.Trash:Add(self.OpenAnim )
+                    self.OpenAnim:PlayAnim(bpAnim)
+					WaitFor(self.OpenAnim)
+						local units = AIUtils.GetOwnUnitsAroundPoint(
 			
 			self:GetAIBrain(), 
 			categories.LASERWALL,
@@ -157,63 +165,99 @@ ALaserFenceUnit = Class(StructureUnit) {
             
 
             for _,unit in units do
-			if unit == nil then
-			else
-            Effekt1 = AttachBeamEntityToEntity(self, 'Effect1', unit, 'Effect1', self:GetArmy(), Effects )
-			Effekt2 = AttachBeamEntityToEntity(self, 'Effect2', unit, 'Effect2', self:GetArmy(), Effects )
-			Effekt3 = AttachBeamEntityToEntity(self, 'Effect3', unit, 'Effect3', self:GetArmy(), Effects )
-            end
-			end
-
-        if bpAnim then		
-			if not self.Dead then
-				    self.OpenAnim = CreateAnimator(self)
-					self.Trash:Add(self.OpenAnim )
-                    self.OpenAnim:PlayAnim(bpAnim)
-					if Desc == 'Laser Fence' then
-					--LOG('Distanz X: ', x)
-					--LOG('Distanz Y: ', y)
-					Dummy = CreateUnitHPR('UABAB0201a',self:GetArmy(), x, 0, y, 0, 0, 0)
-					else
-					end
-					
-            end
-		end
 			
-    end,
-	
-	OnReclaimed = function(self, reclaimer)
-            Effekt1:Destroy()
-			Effekt2:Destroy()
-			Effekt3:Destroy()
-
+					local buildpos = unit:GetPosition()
+		local pos = self:GetPosition()
+		local alpha = math.atan2 (buildpos[3] - pos[3] ,buildpos[1] - pos[1])
+		local dist = VDist2(buildpos[1], buildpos[3], pos[1], pos[3])
+		LOG('Position: ', math.cos(alpha) * dist)
+		LOG('Builder Position: ', math.sin(alpha) * dist)
+		
+		local CheckAlpha = math.cos(alpha) * dist
+		local CheckDist = math.sin(alpha) * dist
+		
+		if CheckAlpha < 0 and CheckDist == 2 then
+		x = buildpos[1] - math.cos(alpha) * dist
+		y = buildpos[3] - math.sin(alpha) * dist + 1
+		Dummy = CreateUnitHPR('UABAB0201a',self:GetArmy(), x, 0, y, 0, 0, 0)
+		elseif CheckAlpha < 0 and CheckDist == -2 then
+		x = buildpos[1] - math.cos(alpha) * dist
+		y = buildpos[3] - math.sin(alpha) * dist - 1
+		Dummy = CreateUnitHPR('UABAB0201a',self:GetArmy(), x, 0, y, 0, 0, 0)
+		elseif CheckAlpha == 2 and CheckDist == 0 then
+		x = buildpos[1] - math.cos(alpha) * dist + 1
+		y = buildpos[3] - math.sin(alpha) * dist
+		Dummy = CreateUnitHPR('UABAB0201a',self:GetArmy(), x, 0, y, 0, 0, 0)
+		elseif CheckAlpha == -2 and CheckDist < 0 then
+		x = buildpos[1] - math.cos(alpha) * dist - 1
+		y = buildpos[3] - math.sin(alpha) * dist
+		Dummy = CreateUnitHPR('UABAB0201a',self:GetArmy(), x, 0, y, 0, 0, 0)
+			end
+		end	
+		
+		end)
+			
     end,
 	
 	OnKilled = function(self)
         StructureUnit.OnKilled(self)
-		Dummy:Destroy()
-		if ChargeEffects01Bag then
-            for k, v in ChargeEffects01Bag do
-                v:Destroy()
+		local units = AIUtils.GetOwnUnitsAroundPoint(
+			
+			self:GetAIBrain(), 
+			categories.LASERWALLDUMMY,
+			self:GetPosition(), 
+			2
+			
+			)
+            
+
+            for _,unit in units do
+			if unit == nil then
+			else
+				unit:Destroy()
             end
-        end
+			end
     end,
 }
 
 ALaserFenceDummyUnit = Class(StructureUnit) {
 	OnStopBeingBuilt = function(self, builder, layer)
         StructureUnit.OnStopBeingBuilt(self, builder, layer)
+		
+        self:HideBone( 0, true )
 
+						local bp = self:GetBlueprint()
+        local bpAnim = bp.Display.AnimationOpen
+					self:ForkThread(function()
+					 self.OpenAnim = CreateAnimator(self)
+					self.Trash:Add(self.OpenAnim )
+                    self.OpenAnim:PlayAnim(bpAnim)
+					WaitFor(self.OpenAnim)
+		
+		
+				local units = AIUtils.GetOwnUnitsAroundPoint(
+			
+			self:GetAIBrain(), 
+			categories.LASERWALL,
+			self:GetPosition(), 
+			2
+			
+			)
+            
+
+            for _,unit in units do
+					table.insert(ChargeEffects01Bag,AttachBeamEntityToEntity(self, 'Effect1', unit, 'Effect1', self:GetArmy(), Effects ))
+					table.insert(ChargeEffects01Bag,AttachBeamEntityToEntity(self, 'Effect2', unit, 'Effect2', self:GetArmy(), Effects ))
+					table.insert(ChargeEffects01Bag,AttachBeamEntityToEntity(self, 'Effect3', unit, 'Effect3', self:GetArmy(), Effects ))
+			end
+			
+			end)
 			
     end,
 	
 	OnKilled = function(self)
         StructureUnit.OnKilled(self)
-		if ChargeEffects01Bag then
-            for k, v in ChargeEffects01Bag do
-                v:Destroy()
-            end
-        end
+		self:Destroy()
     end,
 	
 }
