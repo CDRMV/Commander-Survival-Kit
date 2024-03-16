5a944e45a23f0b5ac333ab16ab4632250c1dce7f
CDRMV <46399086+CDRMV@users.noreply.github.com>
2024-03-16 17:11:34 +0100

    New Aeon Battleship (WIP)

---
+++ mods/Commander Survival Kit Units/units/Aeon/Naval/CSKAS0300/CSKAS0300_script.lua
@@ -0,0 +1,27 @@
+#****************************************************************************
+#**
+#**  File     :  /cdimage/units/UAS0201/UAS0201_script.lua
+#**  Author(s):  John Comes, David Tomandl, Jessica St. Croix
+#**
+#**  Summary  :  Aeon Destroyer Script
+#**
+#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
+#****************************************************************************
+
+local ASeaUnit = import('/lua/aeonunits.lua').ASeaUnit
+local AeonWeapons = import('/lua/aeonweapons.lua')
+local ADFCannonOblivionWeapon = AeonWeapons.ADFCannonOblivionWeapon
+local AANDepthChargeBombWeapon = AeonWeapons.AANDepthChargeBombWeapon
+local AANChronoTorpedoWeapon = AeonWeapons.AANChronoTorpedoWeapon
+local AIFQuasarAntiTorpedoWeapon = AeonWeapons.AIFQuasarAntiTorpedoWeapon
+
+
+CSKAS0300 = Class(ASeaUnit) {
+    BackWakeEffect = {},
+    Weapons = {
+        FrontTurret1 = Class(ADFCannonOblivionWeapon) {},
+		FrontTurret2 = Class(ADFCannonOblivionWeapon) {},
+    },
+}
+
+TypeClass = CSKAS0300
