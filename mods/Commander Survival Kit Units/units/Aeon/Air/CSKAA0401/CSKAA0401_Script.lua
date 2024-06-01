2c0a5ab138bc2a0c5591544a6f6fc1f1a76948e5
CDRMV <46399086+CDRMV@users.noreply.github.com>
2024-06-02 01:17:45 +0200

    Aeon Exp Bomber Update (WIP)

--- mods/Commander Survival Kit Units/units/Aeon/Air/CSKAA0401/CSKAA0401_Script.lua
+++ mods/Commander Survival Kit Units/units/Aeon/Air/CSKAA0401/CSKAA0401_Script.lua
@@ -9,13 +9,57 @@
 #****************************************************************************
 
 local AAirUnit = import('/lua/defaultunits.lua').AirUnit
+local AIFQuanticArtillery = import('/lua/aeonweapons.lua').AIFQuanticArtillery
 local AIFBombQuarkWeapon = import('/lua/aeonweapons.lua').AIFBombQuarkWeapon
-
+local AAAZealotMissileWeapon = import('/lua/aeonweapons.lua').AAAZealotMissileWeapon
 
 CSKAA0401 = Class(AAirUnit) {
+	
     Weapons = {
-        Bomb = Class(AIFBombQuarkWeapon) {},
+        Bomb1 = Class(AIFBombQuarkWeapon) {},
+		Bomb2 = Class(AIFBombQuarkWeapon) {},
+		MainBomb = Class(AIFBombQuarkWeapon) {},
+		AntiAirMissiles = Class(AAAZealotMissileWeapon) {},
     },
+	
+	OnStopBeingBuilt = function(self,builder,layer)
+        AAirUnit.OnStopBeingBuilt(self,builder,layer)
+			self:HideBone('R_WingEffect', true)
+			self:HideBone('L_WingEffect', true)
+			self:ShowBone('Exhaust01', true)
+			self:ShowBone('Exhaust02', true)
+    end,
+	
+	
+	OnCreate = function(self)
+        AAirUnit.OnCreate(self)
+		self:AddToggleCap('RULEUTC_ProductionToggle')
+		self:SetWeaponEnabledByLabel('Bomb1', false)
+		self:SetWeaponEnabledByLabel('Bomb2', false)			
+    end,
+	
+	OnScriptBitSet = function(self, bit)
+        AAirUnit.OnScriptBitSet(self, bit)
+		if bit == 1 then 
+		self:RemoveToggleCap('RULEUTC_ProductionToggle')
+		self:SetWeaponEnabledByLabel('MainBomb', false)	
+		self:SetWeaponEnabledByLabel('Bomb1', true)
+		self:SetWeaponEnabledByLabel('Bomb2', true)
+		end	
+    end,
+	
+	OnScriptBitClear = function(self, bit)
+        AAirUnit.OnScriptBitClear(self, bit)
+		if bit == 1 then 	
+		self:ForkThread(function()  
+			WaitSeconds(10)	
+			self:SetWeaponEnabledByLabel('Bomb1', false)
+			self:SetWeaponEnabledByLabel('Bomb2', false)
+			self:AddToggleCap('RULEUTC_ProductionToggle')
+			self:SetWeaponEnabledByLabel('MainBomb', true)				
+		end)
+		end	
+    end,
 }
 
 TypeClass = CSKAA0401
