4a24c991ebce48a17d5770c9c01f6b411796c7d6
CDRMV <46399086+CDRMV@users.noreply.github.com>
2024-06-09 18:41:57 +0200

    Volcano Savegame Fix + Bunker Range Buff

--- mods/Commander Survival Kit Units/units/Terraform/UTX0401/UTX0401_script.lua
+++ mods/Commander Survival Kit Units/units/Terraform/UTX0401/UTX0401_script.lua
@@ -17,109 +17,48 @@ local R, Ceil = Random, math.ceil
 local Util = import('/lua/utilities.lua')
 local RandomFloat = Util.GetRandomFloat
 
+
 UTX0401 = Class(StructureUnit) {
     Weapons = {
         Turret01 = Class(DefaultProjectileWeapon) {},
     },
 	
-	
-	
-    OnStopBeingBuilt = function(self,builder,layer)
-        StructureUnit.OnStopBeingBuilt(self,builder,layer)
-		--[[
-				self:ForkThread(
+	--[[
+	-- Flattenskirt Load for other purposes
+	OnCreate = function(self)
+        StructureUnit.OnCreate(self)
+		local interval = 0
+		local grow = 0
+		local growing = 0
+								local position = self:GetPosition()
+		local Height = GetTerrainHeight(position[1], position[3])
+			local sqrt, sin, min, log10 = math.sqrt, math.sin, math.min, math.log10		
+			local sX, sZ = math.floor(position[1]-Height), math.floor(position[3]-Height)
+            local eX, eZ = math.ceil(position[1]+Height), math.ceil(position[3]+Height)
+		self:ForkThread(
             function()
 		while true do
-		WaitSeconds(1)	   
-		local orientation = RandomFloat(0,2*math.pi)
-		local position = self:GetPosition()
-		local qx, qy, qz, qw = unpack(self:GetOrientation())
-		CreateDecal(position, orientation, '/mods/Commander Survival Kit Units/textures/volcan_albedo2.dds', '', 'Albedo', 50, 50, 1200, 0, self:GetArmy())
-        self.Effect1 = CreateAttachedEmitter(self,'UTX0400',self:GetArmy(), ModEffectpath .. 'vulcano_smoke_01_emit.bp'):ScaleEmitter(10):OffsetEmitter(0,-10,0)
-        self.Trash:Add(self.Effect1)
-		self.Effect2 = CreateAttachedEmitter(self,'UTX0400',self:GetArmy(), ModEffectpath .. 'vulcano_smoke_01_emit.bp'):ScaleEmitter(10):OffsetEmitter(0,-10,0)
-        self.Trash:Add(self.Effect2)
-		self.Effect3 = CreateAttachedEmitter(self,'UTX0400',self:GetArmy(), ModEffectpath .. 'lava_fontaene_01_emit.bp'):ScaleEmitter(10):OffsetEmitter(0,-10,0)
-        self.Trash:Add(self.Effect3)
-		self.Effect3 = CreateAttachedEmitter(self,'UTX0400',self:GetArmy(), ModEffectpath .. 'lava_fontaene_01_emit.bp'):ScaleEmitter(10):OffsetEmitter(2,-10,0)
-        self.Trash:Add(self.Effect3)
-		self.Effect3 = CreateAttachedEmitter(self,'UTX0400',self:GetArmy(), ModEffectpath .. 'lava_fontaene_01_emit.bp'):ScaleEmitter(10):OffsetEmitter(-2,-10,0)
-        self.Trash:Add(self.Effect3)
-		self.Effect3 = CreateAttachedEmitter(self,'UTX0400',self:GetArmy(), ModEffectpath .. 'lava_fontaene_01_emit.bp'):ScaleEmitter(10):OffsetEmitter(0,-10,2)
-        self.Trash:Add(self.Effect3)
-		self.Effect3 = CreateAttachedEmitter(self,'UTX0400',self:GetArmy(), ModEffectpath .. 'lava_fontaene_01_emit.bp'):ScaleEmitter(10):OffsetEmitter(0,-10,-2)
-        self.Trash:Add(self.Effect3)
-		CreateDecal(position, orientation, '/mods/Commander Survival Kit Units/textures/lavaflow_albedo.dds', '', 'Albedo', 50, 50, 1200, 0, self:GetArmy())
-		CreateDecal(position, orientation, '/mods/Commander Survival Kit Units/textures/lava_albedo.dds', '', 'Albedo', 10, 10, 1200, 0, self:GetArmy())
-		local Deformation = 0 
-		
-		if Deformation == 0 and Terrian then
-			    local bp = self:GetBlueprint()
-                self.AimingNode = CreateRotator(self, 0, 'x', 0, 10000, 10000, 1000)
-                WaitFor(self.AimingNode)
-				local interval = 0
-											local sqrt, sin, min, log10 = math.sqrt, math.sin, math.min, math.log10		
-			local sX, sZ = math.floor(position[1]-height), math.floor(position[3]-height)
-            local eX, eZ = math.ceil(position[1]+height), math.ceil(position[3]+height)
-            for x=sX, eX do    
-                if x<0 or x>ScenarioInfo.size[1] then continue end
-                for z=sZ, eZ do
-                    if z<0 or z>ScenarioInfo.size[2] then continue end
-                    local dSq = VDist2Sq(x, z, position[1], position[3])
-                    if dSq <= height*height then
-                        local relD = sin(1-(sqrt(dSq)/height))
-                        local maxD = min(height*4, log10(566231040))
-                        local curD = GetTerrainHeight(x, z)
-                            local target = curD + (relD*maxD) 
-                                FlattenMapRect(x, z, 0, 0, target)
-							
-						
-                    end
-                end
-            end
-                    local num = Ceil((R()+R()+R()+R()+R()+R()+R()+R()+R()+R()+R())*R(1,10))
-                    coroutine.yield(num)
-                    self:GetWeaponByLabel'Turret01':FireWeapon()
-		    local sX2, sZ2 = math.floor(position[1]-4), math.floor(position[3]-4)
-            local eX2, eZ2 = math.ceil(position[1]+4), math.ceil(position[3]+4)
-            for x=sX2, eX2 do
-                if x<0 or x>ScenarioInfo.size[1] then continue end
-                for z=sZ2, eZ2 do
-                    if z<0 or z>ScenarioInfo.size[2] then continue end
-                    local dSq2 = VDist2Sq(x, z, position[1], position[3])
-                    if dSq2 <= 4*4 then
-                        local relD2 = sin(1-(sqrt(dSq2)/4) + 10)
-                        local maxD2 = min(6, log10(4) - 3)
-                        local curD2 = GetTerrainHeight(x, z)
-						local target2 = curD2-(relD2*maxD2)
-								FlattenMapRect(x, z, 0, 0, target2)
-							end 
-						end
-					end	
+		WaitSeconds(0.1)
+		FlattenMapRect(position[1]-15, position[3]-15, 26, 26, 0)
+		end 
+		end		
+		)		
+    end,
+	]]--
 
-
-				while (interval < 61) do
-
-					if interval == 60 then 
-						interval = 0
-					else
-					local num = Ceil((R()+R()+R()+R()+R()+R()+R()+R()+R()+R()+R())*R(1,10))
-                    coroutine.yield(num)
-                    self:GetWeaponByLabel'Turret01':FireWeapon()
-					end
-				
-				interval = interval + 1
-
-				end	
-			end	
-			end	
-			end			
-			)
-		]]--	
+	
+    OnCreate = function(self)
+        StructureUnit.OnCreate(self)
 		self:ForkThread(
         function()
+		local grow = 0
+		local growing = 0 
+								local position = self:GetPosition()
+		local Height = GetTerrainHeight(position[1], position[3])
+			local sqrt, sin, min, log10 = math.sqrt, math.sin, math.min, math.log10		
+			local sX, sZ = math.floor(position[1]-Height), math.floor(position[3]-Height)
+            local eX, eZ = math.ceil(position[1]+Height), math.ceil(position[3]+Height)
 		local orientation = RandomFloat(0,2*math.pi)
-		local position = self:GetPosition()
 		local qx, qy, qz, qw = unpack(self:GetOrientation())
 		CreateDecal(position, orientation, '/mods/Commander Survival Kit Units/textures/volcan_albedo2.dds', '', 'Albedo', 50, 50, 1200, 0, self:GetArmy())
         self.Effect1 = CreateAttachedEmitter(self,'UTX0400',self:GetArmy(), ModEffectpath .. 'vulcano_smoke_01_emit.bp'):ScaleEmitter(10):OffsetEmitter(0,-10,0)
@@ -146,81 +85,104 @@ UTX0401 = Class(StructureUnit) {
         self.Trash:Add(self.Effect3)
 		CreateDecal(position, orientation, '/mods/Commander Survival Kit Units/textures/lavaflow_albedo.dds', '', 'Albedo', 50, 50, 1200, 0, self:GetArmy())
 		CreateDecal(position, orientation, '/mods/Commander Survival Kit Units/textures/lava_albedo.dds', '', 'Albedo', 10, 10, 1200, 0, self:GetArmy())
-		local MainInterval = 0
+		while true do
+		WaitSeconds(1)
+		while grow >= 7 do 
+		WaitSeconds(1)
 		local height = 18
-		local bp = self:GetBlueprint()
-		if MainInterval == 0 then
-			    local bp = self:GetBlueprint()
-                self.AimingNode = CreateRotator(self, 0, 'x', 0, 10000, 10000, 1000)
-                WaitFor(self.AimingNode)
-				local interval = 0
-				local grow = 0
-                while (interval < 61) do
-					if interval == 10 then
-					local qx, qy, qz, qw = unpack(self:GetOrientation())
-					CreateUnit('UTX0400',1,position[1]+4, position[2]+4, position[3]+4,qx, qy, qz, qw, 0)
-					end 
-					if interval == 60 then 
-						interval = 0
-					else	
-								local sqrt, sin, min, log10 = math.sqrt, math.sin, math.min, math.log10
-			if grow > 8 then
-					local num = Ceil((R()+R()+R()+R()+R()+R()+R()+R()+R()+R()+R())*R(1,10))
-                    coroutine.yield(num)
-                    self:GetWeaponByLabel'Turret01':FireWeapon()
-					else
-			local sX, sZ = math.floor(position[1]-height), math.floor(position[3]-height)
-            local eX, eZ = math.ceil(position[1]+height), math.ceil(position[3]+height)
+			local sX, sZ = math.floor(position[1]-Height), math.floor(position[3]-Height)
+            for x=sX, eX do    
+                if x<0 or x>ScenarioInfo.size[1] then continue end
+                for z=sZ, eZ do
+                    if z<0 or z>ScenarioInfo.size[2] then continue end
+                    local dSq = VDist2Sq(x, z, position[1], position[3])
+                    if dSq <= Height*Height then
+                        local relD = sin(1-(sqrt(dSq)/Height))
+                        local maxD = min(Height*4, log10(566231040))
+                        local curD = Height
+                            local target = curD + (relD*maxD) 
+                                FlattenMapRect(x, z, 0, 0, target)	
+								local Crater = 0 			
+                    local dSq2 = VDist2Sq(x, z, position[1], position[3])
+                    if dSq2 <= 4*4 then
+						if Crater < 60 then							
+                        local relD2 = sin(1-(sqrt(dSq2)/4) + 10)
+                        local maxD2 = min(6, log10(4) - 3)
+                        local curD2 = GetTerrainHeight(position[1] - 4, position[3] + 4)
+						local target2 = curD2 - (relD/maxD) 
+								FlattenMapRect(x, z, 0, 0, target2 -1)
+											Crater = Crater + 1	
+						else			
+						end	
+                    end
+                end
+            end
+		end
+		grow = grow + 1
+		end
+		while grow < 8 do
+		WaitSeconds(1)
             for x=sX, eX do
                 if x<0 or x>ScenarioInfo.size[1] then continue end
                 for z=sZ, eZ do
                     if z<0 or z>ScenarioInfo.size[2] then continue end
                     local dSq = VDist2Sq(x, z, position[1], position[3])
-                    if dSq <= height*height then
-                        local relD = sin(1-(sqrt(dSq)/height))
-                        local maxD = min(height*0.5, log10(height))
+                    if dSq <= 18*18 then
+                        local relD = sin(1-(sqrt(dSq)/18))
+                        local maxD = min(18*0.5, log10(18))
                         local curD = GetTerrainHeight(x, z)
                             local target = curD + (relD*maxD)
                                 FlattenMapRect(x, z, 0, 0, target)
-							
-						
-                    end
-                end
-            end
-                    local num = Ceil((R()+R()+R()+R()+R()+R()+R()+R()+R()+R()+R())*R(1,10))
-                    coroutine.yield(num)
-                    self:GetWeaponByLabel'Turret01':FireWeapon()
-		    local sX2, sZ2 = math.floor(position[1]-4), math.floor(position[3]-4)
-            local eX2, eZ2 = math.ceil(position[1]+4), math.ceil(position[3]+4)
-            for x=sX2, eX2 do
-                if x<0 or x>ScenarioInfo.size[1] then continue end
-                for z=sZ2, eZ2 do
-                    if z<0 or z>ScenarioInfo.size[2] then continue end
+								local Crater = 0 			
                     local dSq2 = VDist2Sq(x, z, position[1], position[3])
                     if dSq2 <= 4*4 then
-                        local relD2 = sin(1-(sqrt(dSq2)/4) + 0.1)
-                        local maxD2 = min(6, log10(4) + 0.1)
-                        local curD2 = GetTerrainHeight(x, z)
-						local target2 = position[2]-(relD2*maxD2)
-							if grow == 0 then
-								FlattenMapRect(x, z, 0, 0, target2)
-							else
-                                FlattenMapRect(x, z, 1, 1, curD2)
-							end	
-						
+						if Crater < 60 then							
+                        local relD2 = sin(1-(sqrt(dSq2)/4) + 10)
+                        local maxD2 = min(6, log10(4) - 3)
+                        local curD2 = GetTerrainHeight(position[1] - 4, position[3] + 4)
+						local target2 = curD2 - (relD/maxD) 
+								FlattenMapRect(x, z, 0, 0, target2 -1)
+											Crater = Crater + 1	
+						else			
+						end	
                     end
                 end
             end
-					grow = grow + 1
-			end		
-					interval = interval + 1
-                end
-            end
-		MainInterval = MainInterval + 1
-		else
-		
-		end	
+
 		end
+		grow = grow + 1
+		end
+		end
+		end	
+		)		
+    end,
+	
+	
+	OnStopBeingBuilt = function(self,builder,layer)
+        StructureUnit.OnStopBeingBuilt(self,builder,layer)
+		local position = self:GetPosition()
+		local qx, qy, qz, qw = unpack(self:GetOrientation())
+		local interval = 0
+        		self:ForkThread(
+        function()
+		self.AimingNode = CreateRotator(self, 0, 'x', 0, 10000, 10000, 1000)
+        WaitFor(self.AimingNode)
+				while true do
+				WaitSeconds(1)
+		LOG(interval)
+		local num = Ceil((R()+R()+R()+R()+R()+R()+R()+R()+R()+R()+R())*R(1,10))
+        coroutine.yield(num)
+        self:GetWeaponByLabel'Turret01':FireWeapon()
+			if interval == 10 then
+				CreateUnit('UTX0400',1,position[1]+4, position[2]+4, position[3]+4,qx, qy, qz, qw, 0)
+			end 
+			if interval == 60 then 
+				interval = 0
+				else
+						interval = interval + 1
+			end
+		end	
+		end	
 		)	
     end,
 
