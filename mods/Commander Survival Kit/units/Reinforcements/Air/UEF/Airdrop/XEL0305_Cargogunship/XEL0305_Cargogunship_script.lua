834f82aaad7d1efcdf28c58aa7e7f5f4e475235c
CDRMV <46399086+CDRMV@users.noreply.github.com>
2024-09-01 19:42:46 +0200

    New Drop Mechanic for UEF Tech 3 Land Reinforcements

---
+++ mods/Commander Survival Kit/units/Reinforcements/Air/UEF/Airdrop/XEL0305_Cargogunship/XEL0305_Cargogunship_script.lua
@@ -0,0 +1,153 @@
+#****************************************************************************
+#**
+#**  File     :  /cdimage/units/UEA0305/UEA0305_script.lua
+#**  Author(s):  John Comes, David Tomandl, Jessica St. Croix
+#**
+#**  Summary  :  UEF Heavy Gunship Script
+#**
+#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
+#****************************************************************************
+
+local TAirUnit = import('/lua/defaultunits.lua').AirUnit
+
+
+XEL0305_Cargogunship = Class(TAirUnit) {
+    
+    EngineRotateBones = {'R_Engine01', 'R_Engine02', 'L_Engine01', 'L_Engine02',},
+    BeamExhaustCruise = '/effects/emitters/gunship_thruster_beam_01_emit.bp',
+    BeamExhaustIdle = '/effects/emitters/gunship_thruster_beam_02_emit.bp',
+    
+    OnStopBeingBuilt = function(self,builder,layer)
+        TAirUnit.OnStopBeingBuilt(self,builder,layer)
+		
+		self:HideBone( 'Tractor_Emitter', true )
+		
+		local position = self:GetPosition()
+		self.Cargo = CreateUnitHPR('XEL0305_Container', self:GetArmy(), position.x, position.y, position.z, 0, 0, 0)
+		self.Cargo:AttachBoneTo('Attachpoint', self, 'Attachpoint_Lrg_01')
+        self.EngineManipulators = {}
+
+        # create the engine thrust manipulators
+        for key, value in self.EngineRotateBones do
+            table.insert(self.EngineManipulators, CreateThrustController(self, 'Thruster', value))
+        end
+
+        # set up the thursting arcs for the engines
+        for key,value in self.EngineManipulators do
+            #                          XMAX, XMIN, YMAX,YMIN, ZMAX,ZMIN, TURNMULT, TURNSPEED
+            value:SetThrustingParam( -0.0, 0.0, -0.25, 0.25, -0.1, 1, 1.0,      0.25 )
+        end
+
+        for k, v in self.EngineManipulators do
+            self.Trash:Add(v)
+        end
+
+    end,
+	
+		OnCreate = function(self)
+        local pos = self.CachePosition or self:GetPosition()
+        local BorderPos, OppBorPos
+
+        local x, z = pos[1] / ScenarioInfo.size[1] - 0.5, pos[3] / ScenarioInfo.size[2] - 0.6
+
+        if math.abs(x) <= math.abs(z) then
+            BorderPos = {pos[1], nil, math.ceil(z) * ScenarioInfo.size[2]}
+            OppBorPos = {pos[1], nil, BorderPos[3]==0 and ScenarioInfo.size[2] or 0}
+            x, z = 1, 0
+        else
+            BorderPos = {math.ceil(x) * ScenarioInfo.size[1], nil, pos[3]}
+            OppBorPos = {BorderPos[1]==0 and ScenarioInfo.size[1] or 0, nil, pos[3]}
+            x, z = 0, 1
+        end
+
+        BorderPos[2] = GetTerrainHeight(BorderPos[1], BorderPos[3])
+        OppBorPos[2] = GetTerrainHeight(OppBorPos[1], OppBorPos[3])
+
+		local position = self.GetNearestPlayablePoint(self,BorderPos)
+		local oppoposition = self.GetNearestPlayablePoint(self,OppBorPos)
+		self.SpawnPosition = position
+        TAirUnit.OnCreate(self)
+    end,
+	
+	GetPlayableArea = function()
+    if ScenarioInfo.MapData.PlayableRect then
+        return ScenarioInfo.MapData.PlayableRect
+    end
+    return {0, 0, ScenarioInfo.size[1], ScenarioInfo.size[2]}
+	end,
+
+	GetNearestPlayablePoint = function(self,position)
+
+    local px, _, pz = unpack(position)
+	
+	if ScenarioInfo.type == 'campaign' then
+	local playableArea = self.GetPlayableArea()
+
+    -- keep track whether the point is actually outside the map
+    local isOutside = false
+
+    if px < playableArea[1] then
+        isOutside = true
+        px = playableArea[1] + 1
+    elseif px > playableArea[3] then
+        isOutside = true
+        px = playableArea[3] - 1
+    end
+
+    if pz < playableArea[2] then
+        isOutside = true
+        pz = playableArea[2] + 1
+    elseif pz > playableArea[4] then
+        isOutside = true
+        pz = playableArea[4] - 1
+    end
+
+    -- if it really is outside the map then we allocate a new vector
+    if isOutside then
+        return {
+            px, 
+            GetTerrainHeight(px, pz),
+            pz
+        }
+
+    end
+
+	elseif ScenarioInfo.type == 'skirmish' then
+	return position
+	else
+	return position
+	end
+end,
+
+    OnTransportDetach = function(self, attachBone, detachedUnit)
+		detachedUnit:TransportAnimation(-1)
+		IssueMove({self}, self.SpawnPosition)
+		ForkThread( function()
+        while not self.Dead do
+            local orders = table.getn(self:GetCommandQueue())
+            if orders > 1 then
+
+            elseif orders == 1 then
+            elseif orders == 0 then
+				self:Destroy()
+            end
+		WaitSeconds(1)	
+        end
+		end
+        )
+    end,
+
+    # When one of our attached units gets killed, detach it
+    OnAttachedKilled = function(self, attached)
+        attached:DetachFrom()
+    end,
+
+    OnKilled = function(self, instigator, type, overkillRatio)
+        TAirUnit.OnKilled(self, instigator, type, overkillRatio)
+        # TransportDetachAllUnits takes 1 bool parameter. If true, randomly destroys some of the transported
+        # units, otherwise successfully detaches all.
+        self:TransportDetachAllUnits(true)
+    end,
+
+}
+TypeClass = XEL0305_Cargogunship
