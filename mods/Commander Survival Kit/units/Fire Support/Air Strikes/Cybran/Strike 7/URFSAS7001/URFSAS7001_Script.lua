da5cb61f2902f876251696bcc2c5ca28be359d48
CDRMV <46399086+CDRMV@users.noreply.github.com>
2024-12-11 11:22:43 +0100

    In Development: Callable Heavy Bomber Air Strike

---
+++ mods/Commander Survival Kit/units/Fire Support/Air Strikes/Cybran/Strike 7/URFSAS7001/URFSAS7001_Script.lua
@@ -0,0 +1,15 @@
+local CallTorpedoAirStrikeBeacon = import('/lua/defaultunits.lua').CallTorpedoAirStrikeBeacon
+
+URFSAS7001 = Class(CallTorpedoAirStrikeBeacon) {
+    FxTransportBeacon = {'/effects/emitters/red_beacon_light_01_emit.bp'},
+    FxTransportBeaconScale = 1,
+
+    OnCreate = function(self)
+        CallTorpedoAirStrikeBeacon.OnCreate(self)
+        for k, v in self.FxTransportBeacon do
+            self.Trash:Add(CreateAttachedEmitter(self, 0,self:GetArmy(), v):ScaleEmitter(self.FxTransportBeaconScale))
+        end
+    end,
+}
+
+TypeClass = URFSAS7001
