7d1c7961b97ce4b6e94599b134edfa792be64ae5
CDRMV <46399086+CDRMV@users.noreply.github.com>
2025-01-09 22:24:32 +0100

    Air Strike Spawn Fix for all Gameversions

---
+++ mods/Commander Survival Kit/hook/maps/SCMP_014/SCMP_014_script.lua
@@ -0,0 +1,11 @@
+local ScenarioUtils = import('/lua/sim/ScenarioUtilities.lua')
+local ScenarioFramework = import('/lua/ScenarioFramework.lua')
+
+function OnPopulate()
+    ScenarioUtils.InitializeArmies()
+    ScenarioFramework.SetPlayableArea('AREA_1', true)
+end
+
+function OnStart(self)
+
+end
