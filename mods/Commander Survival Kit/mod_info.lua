de48b9c5b1b8c69274b3da6c96a25683e25a61af
CDRMV <cdrmv2017@gmail.com>
2025-02-04 16:31:53 +0100

    Small Background Stuff + Cover

--- mods/Commander Survival Kit/mod_info.lua
+++ mods/Commander Survival Kit/mod_info.lua
@@ -1,11 +1,11 @@
 name = "Commander Survival Kit"
-version = 18
-copyright = "Copyright © 2022 - 2023, CDRMV"
+version = 20
+copyright = "Copyright © 2022 - 2025, CDRMV"
 author = "CDRMV"
 description = "A Sim Mod, which adds the Reinforcement and Fire Support Manager into the Game."
 url = "https://www.moddb.com/mods/commander-survival-kit-csk"
 
-uid = "5t3edt-btz6-9437-h6ui-967gt56fa8118"
+uid = "5t3edt-btz6-9437-h6ui-967gt56fa8120"
 exclusive = false
 ui_only = false
 sim_only = true
