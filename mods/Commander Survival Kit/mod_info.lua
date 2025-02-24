b40ea89877ab8b493a09e56687bce6fc37ae2bb5
CDRMV <cdrmv2017@gmail.com>
2025-02-24 10:53:06 +0100

    Change the Mod ID back to the old one

--- mods/Commander Survival Kit/mod_info.lua
+++ mods/Commander Survival Kit/mod_info.lua
@@ -5,7 +5,7 @@ author = "CDRMV"
 description = "A Sim Mod, which adds the Reinforcement and Fire Support Manager into the Game."
 url = "https://www.moddb.com/mods/commander-survival-kit-csk"
 
-uid = "5t3edt-btz6-9437-h6ui-967gt56fa8120"
+uid = "5t3edt-btz6-9437-h6ui-967gt56fa8118"
 exclusive = false
 ui_only = false
 sim_only = true
