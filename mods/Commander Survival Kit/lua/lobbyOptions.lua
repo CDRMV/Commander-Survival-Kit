b904605d7f380b77dfdde11095d3589e2d1d35ea
CDRMV <cdrmv2017@gmail.com>
2025-02-25 13:40:19 +0100

    New Early Access Update

--- mods/Commander Survival Kit/lua/lobbyOptions.lua
+++ mods/Commander Survival Kit/lua/lobbyOptions.lua
@@ -1,4 +1,266 @@
-LobbyGlobalOptions = {
+
+do
+
+teamOptions =
+{
+    {
+        default = 1,
+        label = "<LOC lobui_0088>Spawn",
+        help = "<LOC lobui_0089>Determine what positions players spawn on the map",
+        key = 'TeamSpawn',
+        pref = 'Lobby_Team_Spawn',
+        values = {
+            {
+                text = "<LOC lobui_0090>Random",
+                help = "<LOC lobui_0091>Spawn everyone in random locations",
+                key = 'random',
+            },
+            {
+                text = "<LOC lobui_0092>Fixed",
+                help = "<LOC lobui_0093>Spawn everyone in fixed locations (determined by slot)",
+                key = 'fixed',
+            },
+        },
+    },
+    {
+        default = 1,
+        label = "<LOC lobui_0096>Team",
+        help = "<LOC lobui_0097>Determines if players may switch teams while in game",
+        key = 'TeamLock',
+        pref = 'Lobby_Team_Lock',
+        values = {
+            {
+                text = "<LOC lobui_0098>Locked",
+                help = "<LOC lobui_0099>Teams are locked once play begins",
+                key = 'locked',
+            },
+            {
+                text = "<LOC lobui_0100>Unlocked",
+                help = "<LOC lobui_0101>Players may switch teams during play",
+                key = 'unlocked',
+            },
+        },
+    },
+}
+
+globalOpts = {
+    {
+        default = 2,
+        label = "<LOC lobui_0102>Unit Cap",
+        help = "<LOC lobui_0103>Set the maximum number of units that can be in play",
+        key = 'UnitCap',
+        pref = 'Lobby_Gen_Cap',
+        values = {
+            {
+                text = "<LOC lobui_0170>250",
+                help = "<LOC lobui_0171>250 units per player may be in play",
+                key = '250',
+            },
+            {
+                text = "<LOC lobui_0172>500",
+                help = "<LOC lobui_0173>500 units per player may be in play",
+                key = '500',
+            },
+            {
+                text = "<LOC lobui_0174>750",
+                help = "<LOC lobui_0175>750 units per player may be in play",
+                key = '750',
+            },
+            {
+                text = "<LOC lobui_0235>1000",
+                help = "<LOC lobui_0236>1000 units per player may be in play",
+                key = '1000',
+            },
+        },
+    },
+    {
+        default = 1,
+        label = "<LOC lobui_0112>Fog of War",
+        help = "<LOC lobui_0113>Set up how fog of war will be visualized",
+        key = 'FogOfWar',
+        pref = 'Lobby_Gen_Fog',
+        values = {
+            {
+                text = "<LOC lobui_0114>Explored",
+                help = "<LOC lobui_0115>Terrain revealed, but units still need recon data",
+                key = 'explored',
+            },
+            {
+                text = "<LOC lobui_0118>None",
+                help = "<LOC lobui_0119>All terrain and units visible",
+                key = 'none',
+            },
+        },
+    },
+    {
+        default = 1,
+        label = "<LOC lobui_0120>Victory Condition",
+        help = "<LOC lobui_0121>Determines how a victory can be achieved",
+        key = 'Victory',
+        pref = 'Lobby_Gen_Victory',
+        values = {
+            {
+                text = "<LOC lobui_0122>Assassination",
+                help = "<LOC lobui_0123>Game ends when commander is destroyed",
+                key = 'demoralization',
+            },
+            {
+                text = "<LOC lobui_0124>Supremacy",
+                help = "<LOC lobui_0125>Game ends when all structures, commanders and engineers are destroyed",
+                key = 'domination',
+            },
+            {
+                text = "<LOC lobui_0126>Annihilation",
+                help = "<LOC lobui_0127>Game ends when all units are destroyed",
+                key = 'eradication',
+            },
+            {
+                text = "<LOC lobui_0128>Sandbox",
+                help = "<LOC lobui_0129>Game never ends",
+                key = 'sandbox',
+            },
+        },
+    },
+    {
+        default = 2,
+        label = "<LOC lobui_0242>Timeouts",
+        help = "<LOC lobui_0243>Sets the number of timeouts each player can request",
+        key = 'Timeouts',
+        pref = 'Lobby_Gen_Timeouts',
+        mponly = true,
+        values = {
+            {
+                text = "<LOC lobui_0244>None",
+                help = "<LOC lobui_0245>No timeouts are allowed",
+                key = '0',
+            },
+            {
+                text = "<LOC lobui_0246>Three",
+                help = "<LOC lobui_0247>Each player has three timeouts",
+                key = '3',
+            },
+            {
+                text = "<LOC lobui_0248>Infinite",
+                help = "<LOC lobui_0249>There is no limit on timeouts",
+                key = '-1',
+            },
+        },
+    },
+    {
+        default = 1,
+        label = "<LOC lobui_0258>Game Speed",
+        help = "<LOC lobui_0259>Set the game speed",
+        key = 'GameSpeed',
+        pref = 'Lobby_Gen_GameSpeed',
+        values = {
+            {
+                text = "<LOC lobui_0260>Normal",
+                help = "<LOC lobui_0261>Fixed at the normal game speed (+0)",
+                key = 'normal',
+            },
+            {
+                text = "<LOC lobui_0262>Fast",
+                help = "<LOC lobui_0263>Fixed at a fast game speed (+4)",
+                key = 'fast',
+            },
+            {
+                text = "<LOC lobui_0264>Adjustable",
+                help = "<LOC lobui_0265>Adjustable in-game",
+                key = 'adjustable',
+            },
+        },
+    },
+    {
+        default = 1,
+        label = "<LOC lobui_0208>Cheating",
+        help = "<LOC lobui_0209>Enable cheat codes",
+        key = 'CheatsEnabled',
+        pref = 'Lobby_Gen_CheatsEnabled',
+        values = {
+            {
+                text = "<LOC _Off>Off",
+                help = "<LOC lobui_0210>Cheats disabled",
+                key = 'false',
+            },
+            {
+                text = "<LOC _On>On",
+                help = "<LOC lobui_0211>Cheats enabled",
+                key = 'true',
+            },
+        },
+    },
+    {
+        default = 1,
+        label = "<LOC lobui_0291>Civilians",
+        help = "<LOC lobui_0292>Set how civilian units are used",
+        key = 'CivilianAlliance',
+        pref = 'Lobby_Gen_Civilians',
+        values = {
+            {
+                text = "<LOC lobui_0293>Enemy",
+                help = "<LOC lobui_0294>Civilians are enemies of players",
+                key = 'enemy',
+            },
+            {
+                text = "<LOC lobui_0295>Neutral",
+                help = "<LOC lobui_0296>Civilians are neutral to players",
+                key = 'neutral',
+            },
+            {
+                text = "<LOC lobui_0297>None",
+                help = "<LOC lobui_0298>No Civilians on the battlefield",
+                key = 'removed',
+            },
+        },
+    },
+    {
+        default = 1,
+        label = "<LOC lobui_0310>Prebuilt Units",
+        help = "<LOC lobui_0311>Set whether the game starts with prebuilt units or not",
+        key = 'PrebuiltUnits',
+        pref = 'Lobby_Prebuilt_Units',
+        values = {
+            {
+                text = "<LOC lobui_0312>Off",
+                help = "<LOC lobui_0313>No prebuilt units",
+                key = 'Off',
+            },
+            {
+                text = "<LOC lobui_0314>On",
+                help = "<LOC lobui_0315>Prebuilt units set",
+                key = 'On',
+            },
+        },
+    },
+    {
+        default = 1,
+        label = "<LOC lobui_0316>No Rush Option",
+        help = "<LOC lobui_0317>Enforce No Rush rules for a certain period of time",
+        key = 'NoRushOption',
+        pref = 'Lobby_NoRushOption',
+        values = {
+            {
+                text = "<LOC lobui_0318>Off",
+                help = "<LOC lobui_0319>Rules not enforced",
+                key = 'Off',
+            },
+            {
+                text = "<LOC lobui_0320>5",
+                help = "<LOC lobui_0321>Rules enforced for 5 mins",
+                key = '5',
+            },
+            {
+                text = "<LOC lobui_0322>10",
+                help = "<LOC lobui_0323>Rules enforced for 10 mins",
+                key = '10',
+            },
+            {
+                text = "<LOC lobui_0324>20",
+                help = "<LOC lobui_0325>Rules enforced for 20 mins",
+                key = '20',
+            },
+        },
+    },
 	{
         default = 1,
         label = "Drop Turrets and Devices",
@@ -550,3 +812,6 @@ LobbyGlobalOptions = {
         },
 	},
 }
+
+
+end
