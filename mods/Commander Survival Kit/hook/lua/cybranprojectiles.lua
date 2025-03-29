a8f6969033b87c5df317ebc1c84dc24a5b03163e
CDRMV <cdrmv2017@gmail.com>
2025-03-29 17:09:32 +0100

    Small Loud Fix for the new Cybran callable Hack Pods

---
+++ mods/Commander Survival Kit/hook/lua/cybranprojectiles.lua
@@ -0,0 +1,12 @@
+CDFBrackmanHackPegProjectile02 = Class(MultiPolyTrailProjectile) {
+    FxImpactTrajectoryAligned = false,
+    PolyTrails = EffectTemplate.CBrackmanCrabPegTrails,
+	PolyTrailOffset = {0,0}, 
+	
+    FxTrails = {},
+    FxImpactUnit = {},
+    FxImpactProp = {},
+    FxImpactLand = EffectTemplate.CBrackmanCrabPegHit01,
+    FxTrailOffset = 0,
+    FxImpactUnderWater = {},
+}
