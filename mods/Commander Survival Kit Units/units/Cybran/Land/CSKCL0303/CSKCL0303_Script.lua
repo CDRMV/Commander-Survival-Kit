2e8d55edb5a0de4bb01c03496a6544cfa9edc1d6
CDRMV <cdrmv2017@gmail.com>
2025-11-16 11:10:41 +0100

    CSK Units: Small Patch for 3 Units

--- mods/Commander Survival Kit Units/units/Cybran/Land/CSKCL0303/CSKCL0303_Script.lua
+++ mods/Commander Survival Kit Units/units/Cybran/Land/CSKCL0303/CSKCL0303_Script.lua
@@ -20,6 +20,17 @@ CSKCL0303 = Class(CWalkingLandUnit)
     Weapons = {
 		MainGun = Class(CDFLaserDisintegratorWeapon) {},
 		MissileRack = Class(CIFMissileLoaWeapon) {},
+		MissileRack2 = Class(CIFMissileLoaWeapon) {
+		    OnWeaponFired = function(self)
+				self:ForkThread(function()
+                self.unit:RemoveTacticalSiloAmmo(5)
+				WaitSeconds(8)
+				if self.unit:GetTacticalSiloAmmoCount() == 0 then
+				self.unit:GiveTacticalSiloAmmo(5)
+				end
+				end)
+            end,
+		},	
 		Bite = Class(CDFLaserPulseLightWeapon) {
            -- FxMuzzleFlash = EffectTemplate.CElectronBolterMuzzleFlash01,
            -- FxMuzzleFlashScale = 0.4,
@@ -32,11 +43,14 @@ CSKCL0303 = Class(CWalkingLandUnit)
 	OnStopBeingBuilt = function(self,builder,layer)
         CWalkingLandUnit.OnStopBeingBuilt(self,builder,layer)
 		local wep1 = self:GetWeaponByLabel('Bite')
-		wep1:SetEnabled(false)	
+		wep1:SetWeaponEnabled(false)	
 		local wep2 = self:GetWeaponByLabel('MainGun')
-		wep2:SetEnabled(true)
-				local wep3 = self:GetWeaponByLabel('MissileRack')
-		wep3:SetEnabled(true)
+		wep2:SetWeaponEnabled(true)
+		local wep3 = self:GetWeaponByLabel('MissileRack')
+		wep3:SetWeaponEnabled(true)
+		local wep4 = self:GetWeaponByLabel('MissileRack2')
+		wep4:SetWeaponEnabled(false)
+		self:SetAutoMode(true)
     end,
 	
 	BiteAnimation = function(self)
@@ -54,28 +68,67 @@ CSKCL0303 = Class(CWalkingLandUnit)
 	OnScriptBitSet = function(self, bit)
         CWalkingLandUnit.OnScriptBitSet(self, bit)
         if bit == 1 then 
+		self:RemoveToggleCap('RULEUTC_SpecialToggle')
 		local wep1 = self:GetWeaponByLabel('Bite')
-		wep1:SetEnabled(true)	
+		wep1:SetWeaponEnabled(true)	
 		local wep2 = self:GetWeaponByLabel('MainGun')
-		wep2:SetEnabled(false)
+		wep2:SetWeaponEnabled(false)
+		if GetScriptBit('RULEUTC_SpecialToggle') == true then
+		self:SetAutoMode(false)
 		local wep3 = self:GetWeaponByLabel('MissileRack')
-		wep3:SetEnabled(false)
-            self:GetWeaponManipulatorByLabel('Bite'):SetHeadingPitch( self:GetWeaponManipulatorByLabel('MainGun'):GetHeadingPitch() )
-        end
+		wep3:SetWeaponEnabled(false)
+		local wep4 = self:GetWeaponByLabel('MissileRack2')
+		wep4:SetWeaponEnabled(true)
+		self:SetAutoMode(false)
+		else
+		local wep3 = self:GetWeaponByLabel('MissileRack')
+		wep3:SetWeaponEnabled(true)
+		local wep4 = self:GetWeaponByLabel('MissileRack2')
+		wep4:SetWeaponEnabled(false)
+		self:SetAutoMode(true)
+		end
+        self:GetWeaponManipulatorByLabel('Bite'):SetHeadingPitch( self:GetWeaponManipulatorByLabel('MainGun'):GetHeadingPitch() )
 		self.CheckCloseTargetsThreadHandle = self:ForkThread(self.CheckCloseTargetsThread)
+		elseif bit == 7 then
+		self:RemoveCommandCap('RULEUCC_SiloBuildTactical')
+		self:RemoveCommandCap('RULEUCC_Tactical')
+		local wep3 = self:GetWeaponByLabel('MissileRack')
+		wep3:SetWeaponEnabled(false)
+		local wep4 = self:GetWeaponByLabel('MissileRack2')
+		wep4:SetWeaponEnabled(true)
+        end
     end,
 
     OnScriptBitClear = function(self, bit)
         CWalkingLandUnit.OnScriptBitClear(self, bit)
 		KillThread(self.CheckCloseTargetsThreadHandle)
         if bit == 1 then 
+		self:AddToggleCap('RULEUTC_SpecialToggle')
 		local wep1 = self:GetWeaponByLabel('Bite')
-		wep1:SetEnabled(false)	
+		wep1:SetWeaponEnabled(false)	
 		local wep2 = self:GetWeaponByLabel('MainGun')
-		wep2:SetEnabled(true)
+		wep2:SetWeaponEnabled(true)
+		if GetScriptBit('RULEUTC_SpecialToggle') == true then
 		local wep3 = self:GetWeaponByLabel('MissileRack')
-		wep3:SetEnabled(true)
-            self:GetWeaponManipulatorByLabel('MainGun'):SetHeadingPitch( self:GetWeaponManipulatorByLabel('Bite'):GetHeadingPitch() )
+		wep3:SetWeaponEnabled(false)
+		local wep4 = self:GetWeaponByLabel('MissileRack2')
+		wep4:SetWeaponEnabled(true)
+		self:SetAutoMode(false)
+		else
+		local wep3 = self:GetWeaponByLabel('MissileRack')
+		wep3:SetWeaponEnabled(true)
+		local wep4 = self:GetWeaponByLabel('MissileRack2')
+		wep4:SetWeaponEnabled(false)
+		self:SetAutoMode(true)
+		end
+        self:GetWeaponManipulatorByLabel('MainGun'):SetHeadingPitch( self:GetWeaponManipulatorByLabel('Bite'):GetHeadingPitch() )
+		elseif bit == 7 then
+		self:AddCommandCap('RULEUCC_SiloBuildTactical')
+		self:AddCommandCap('RULEUCC_Tactical')
+		local wep3 = self:GetWeaponByLabel('MissileRack')
+		wep3:SetWeaponEnabled(true)
+		local wep4 = self:GetWeaponByLabel('MissileRack2')
+		wep4:SetWeaponEnabled(false)
         end
     end,
 	
--- mods/Commander Survival Kit Units/units/Seraphim/Land/CSKSL0300/CSKSL0300_Script.lua
+++ mods/Commander Survival Kit Units/units/Seraphim/Land/CSKSL0300/CSKSL0300_Script.lua
@@ -41,7 +41,80 @@ CSKSL0300 = Class(SWalkingLandUnit) {
                     ChangeState(self, self.IdleState)
                 end
             end,
+        },
+		MissileRack2 = Class(SLaanseMissileWeapon) {
+		
+			OnWeaponFired = function(self)
+				self:ForkThread(function()
+                self.unit:RemoveTacticalSiloAmmo(2)
+				WaitSeconds(8)
+				if self.unit:GetTacticalSiloAmmoCount() == 0 then
+				self.unit:GiveTacticalSiloAmmo(2)
+				end
+				end)
+            end,
+			
+            OnLostTarget = function(self)
+                self:ForkThread( self.LostTargetThread )
+            end,
+            
+            RackSalvoFiringState = State(SLaanseMissileWeapon.RackSalvoFiringState) {
+                OnLostTarget = function(self)
+                    self:ForkThread( self.LostTargetThread )
+                end,            
+            },            
+
+            LostTargetThread = function(self)
+                while not self.unit:IsDead() and self.unit:IsUnitState('Busy') do
+                    WaitSeconds(2)
+                end
+                
+                if self.unit:IsDead() then
+                    return
+                end
+                
+                local bp = self:GetBlueprint()
+
+                if bp.WeaponUnpacks then
+                    ChangeState(self, self.WeaponPackingState)
+                else
+                    ChangeState(self, self.IdleState)
+                end
+            end,
         }
     },
+	
+	OnStopBeingBuilt = function(self,builder,layer)
+        SWalkingLandUnit.OnStopBeingBuilt(self,builder,layer)
+		local wep1 = self:GetWeaponByLabel('MissileRack')
+		wep1:SetWeaponEnabled(true)	
+		local wep2 = self:GetWeaponByLabel('MissileRack2')
+		wep2:SetWeaponEnabled(false)
+		self:SetAutoMode(true)
+    end,
+	
+	OnScriptBitSet = function(self, bit)
+        SWalkingLandUnit.OnScriptBitSet(self, bit)
+        if bit == 1 then 
+		self:RemoveCommandCap('RULEUCC_SiloBuildTactical')
+		self:RemoveCommandCap('RULEUCC_Tactical')
+		local wep3 = self:GetWeaponByLabel('MissileRack')
+		wep3:SetWeaponEnabled(false)
+		local wep4 = self:GetWeaponByLabel('MissileRack2')
+		wep4:SetWeaponEnabled(true)
+		end
+    end,
+
+    OnScriptBitClear = function(self, bit)
+        SWalkingLandUnit.OnScriptBitClear(self, bit)
+        if bit == 1 then 
+		self:AddCommandCap('RULEUCC_SiloBuildTactical')
+		self:AddCommandCap('RULEUCC_Tactical')
+		local wep3 = self:GetWeaponByLabel('MissileRack')
+		wep3:SetWeaponEnabled(true)
+		local wep4 = self:GetWeaponByLabel('MissileRack2')
+		wep4:SetWeaponEnabled(false)
+		end
+    end,
 }
 TypeClass = CSKSL0300
