8679a37405ed8b7e6ed6a2557a0993e9386550f6
CDRMV <cdrmv2017@gmail.com>
2026-03-09 19:12:34 +0100

    CSK Units: Eradicator Patch

--- mods/Commander Survival Kit Units/units/Addon/Strutman/Cybran/SRL0200/SRL0200_script.lua
+++ mods/Commander Survival Kit Units/units/Addon/Strutman/Cybran/SRL0200/SRL0200_script.lua
@@ -12,7 +12,7 @@ local CWalkingLandUnit = import('/lua/defaultunits.lua').WalkingLandUnit
 local CybranWeaponsFile = import('/lua/cybranweapons.lua')
 local CDFRocketIridiumWeapon = CybranWeaponsFile.CDFRocketIridiumWeapon
 local EffectUtil = import('/lua/EffectUtilities.lua')
-
+local CDFParticleCannonWeapon = import('/lua/cybranweapons.lua').CDFParticleCannonWeapon
 
 SRL0200 = Class(CWalkingLandUnit) {
     Weapons = {
@@ -25,25 +25,39 @@ SRL0200 = Class(CWalkingLandUnit) {
 			self.unit.Launch = false			
 			end)
 		end,
-		},
-		MissileDirectWeapon = Class(CDFRocketIridiumWeapon) {
-		OnWeaponFired = function(self)
-			ForkThread( function()	
-			self.unit.Launch = true
-			WaitSeconds(0.3)
-			self.unit:HideBone('Scud', true)    
-			self.unit.Launch = false			
-			end)
+		
+		PlayFxWeaponUnpackSequence = function(self)
+			if self.unit.number == 0 then
+            local unpackAnimator = CreateAnimator(self.unit)
+            self.UnpackAnimator = unpackAnimator
+			if self.unit:GetScriptBit('RULEUTC_WeaponToggle') == false then
+			self.UnpackAnimator:PlayAnim('/mods/Commander Survival Kit Units/units/Addon/Strutman/Cybran/SRL0200/SRL0200_aunpack.sca'):SetRate(0.2)
+			else
+			self.UnpackAnimator:PlayAnim('/mods/Commander Survival Kit Units/units/Addon/Strutman/Cybran/SRL0200/SRL0200_adirectunpack.sca'):SetRate(0.2)
+			end
+			self.WeaponPackState = 'Unpacking'
+            WaitFor(unpackAnimator)
+			self.unit.number = 1
+			end
 		end,
+		
+		PlayFxWeaponPackSequence = function(self)
+                self.UnpackAnimator:SetRate(-0.2)
+
+            self.WeaponPackState = 'Packing'
+            WaitFor(self.UnpackAnimator)
+			self.unit.number = 0
+        self.WeaponPackState = 'Packed'
+    end,
 		},
+		MainGun = Class(CDFParticleCannonWeapon) {},
     },
 	
 	OnCreate = function(self)
         CWalkingLandUnit.OnCreate(self)
+		self.number = 0
 		self.MissileWeapon = self:GetWeaponByLabel('MissileWeapon')
-		self.MissileDirectWeapon = self:GetWeaponByLabel('MissileDirectWeapon')
-		self.MissileWeapon:SetEnabled(true)
-		self.MissileDirectWeapon:SetEnabled(false)
+		self.MainGun = self:GetWeaponByLabel('MainGun')
 		ForkThread( function()	
 		self.LaunchEffect = false
 		while true do
@@ -61,16 +75,14 @@ SRL0200 = Class(CWalkingLandUnit) {
 	OnScriptBitSet = function(self, bit)
         CWalkingLandUnit.OnScriptBitSet(self, bit)
         if bit == 1 then 
-		self.MissileWeapon:SetEnabled(false)
-		self.MissileDirectWeapon:SetEnabled(true)
+		self.MainGun:SetEnabled(false)
         end
     end,
 
     OnScriptBitClear = function(self, bit)
         CWalkingLandUnit.OnScriptBitClear(self, bit)
         if bit == 1 then 
-		self.MissileWeapon:SetEnabled(true)
-		self.MissileDirectWeapon:SetEnabled(false)
+		self.MainGun:SetEnabled(true)
         end
     end,
 	
